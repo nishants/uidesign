7b550a39f98d7d0d5995faf76088100dd08fe22a
nishant <nishant.singh87@gmail.com>
2015-09-20 22:36:40 +0530

    creating angular directive.

--- shutter-menu.js
+++ shutter-menu.js
@@ -1,13 +1,44 @@
 (function () {
   "use strict"
   var module = angular.module("shutter-menu", []);
-  module.directive("shutterMenuBar", [function () {
+  module.service("shutterMenu", [function () {
+
+    var
+        init = function (menu) {
+          this.menu = menu;
+        },
+        showAppMenu = function () {
+          this.menu.topNavigation();
+        },
+        showSubMenu = function () {
+          this.menu.contextNavigation();
+        },
+        show = function () {
+          this.menu.show();
+        },
+        hide = function () {
+          this.menu.hide();
+        };
+
+    return {
+      init: init,
+      show: show,
+      hide: hide,
+      showAppMenu: showAppMenu,
+      showSubMenu: showSubMenu,
+    };
+  }]);
+  module.directive("shutterMenuBar", ["shutterMenu", function (shutterMenu) {
     return {
       restrict: 'A',
       transclude: false,
       scope: false,
-      controller: function ($scope) {
-        alert($scope.sayLoud);
+      link: function (scope, element, attrs) {
+        var bottomBarClass = attrs["bottomBarClass"],
+            topBarClass = attrs["topBarClass"];
+
+        var menu = new Menu($("#title-bar"), $("#app-menu"), $("#context-menu"), topBarClass, bottomBarClass);
+        shutterMenu.init(menu);
       }
     };
   }]);
