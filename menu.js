367ac2bea3fcfafe0de322bb0802b398086a9e2b
nishant <nishant.singh87@gmail.com>
2015-09-20 19:47:05 +0530

    adding auto hide for app context.

--- menu.js
+++ menu.js
@@ -8,20 +8,42 @@
           top: yPosition,
         }, animationDuration).promise();
       },
-
-      Menu = function ($titleBar, $appMenu, $contextMenu) {
+      totalHeightOf = function($e){
+        return $e.height() + parseInt($e.css("padding-bottom")) + parseInt($e.css("padding-top"));
+      },
+      Menu = function ($titleBar, $appMenu, $contextMenu, topBarClass, bottomBarClass) {
         this.$menuContainer = $titleBar;
         this.$topMenu = $appMenu;
         this.$bottomMenu = $contextMenu;
+
+        this.topBarClass    = topBarClass;
+        this.bottomBarClass = bottomBarClass;
+
         this.containerHeight = $titleBar.height();
       };
 
   Menu.prototype.topNavigation = function () {
     var self = this;
     self.hide().done(function () {
+      self.$topMenu.removeClass(self.bottomBarClass);
+      self.$topMenu.addClass(self.topBarClass);
+
+      self.$bottomMenu.removeClass(self.topBarClass);
+      self.$bottomMenu.addClass(self.bottomBarClass);
       return self.$topMenu.insertBefore(self.$bottomMenu);
     }).done(function () {
+      return moveToY(self.$bottomMenu, -totalHeightOf(self.$bottomMenu));
+    }).done(function () {
       self.show();
+    });
+
+    self.$bottomMenu.off();
+    self.$topMenu.off();
+
+    this.$menuContainer.hover(function(){
+      moveToY(self.$bottomMenu, 0)
+    }, function(){
+      moveToY(self.$bottomMenu, -totalHeightOf(self.$bottomMenu))
     });
   };
 
