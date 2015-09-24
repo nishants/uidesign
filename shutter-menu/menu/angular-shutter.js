29cc8c8fbdbce42ba752b5a26c142927618cc398
nishant <nishant.singh87@gmail.com>
2015-09-25 02:36:38 +0530

    show menu on scroll up, hide on scrolldown.

--- shutter-menu/menu/angular-shutter.js
+++ shutter-menu/menu/angular-shutter.js
@@ -29,6 +29,35 @@
     };
   }]);
   module.directive("shutterMenu", ["shutterMenu", function (shutterMenu) {
+    var ScrollListener = function(onScrollUp, onScrollDown){
+      var lastPosition = 0,
+          lastUp = false,
+          onUp = function () {
+            lastUp = true;
+            onScrollUp();
+          },
+          onDown = function () {
+            lastUp = false;
+            onScrollDown();
+          },
+          doNothing = function (){};
+
+      var scrolledTo = function(position) {
+        var goingUp = position < lastPosition
+        lastPosition = position;
+
+        goingUp != lastUp ? (goingUp ? onUp()  : onDown()) : doNothing();
+      };
+
+      $(window).load(function () {
+        $(window).on("scroll", function () {
+          scrolledTo(window.pageYOffset);
+        });
+
+      });
+    };
+
+;
     return {
       restrict: 'A',
       transclude: false,
@@ -39,14 +68,19 @@
             menuContainer = $(element),
             bars = menuContainer.find("[shutter-menu-bar]"),
             $menu = $(bars[0]),
-            $submenu = $(bars[1]);
-
-        shutterMenu.init(
-            new Menu(
+            $submenu = $(bars[1]),
+            menu = new Menu(
                 new MenuContainer(menuContainer),
                 new MenuBar($menu, topBarClass, bottomBarClass),
-                new MenuBar($submenu, topBarClass, bottomBarClass))
-        );
+                new MenuBar($submenu, topBarClass, bottomBarClass));
+
+
+        shutterMenu.init(menu);
+        new ScrollListener(function(){
+          menu.show();
+        }, function(){
+          menu.hide();
+        });
       }
     };
   }]);
