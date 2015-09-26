637fd6217f0deb815b0b902b7a95a15917629454
nishant <nishant.singh87@gmail.com>
2015-09-26 16:14:47 +0530

    adding on mouse press behavior to buttons.

--- shutter-menu/menu/angular-shutter.js
+++ shutter-menu/menu/angular-shutter.js
@@ -127,4 +127,15 @@
       }
     };
   }]);
+  module.directive("button", [function(){
+    return {
+      restrict: 'E',
+      transclude: false,
+      scope: false,
+      link: function(scope, element){
+        element.on("mousedown", function(){element.addClass("pressed-button");;})
+        element.on("mouseup", function(){element.removeClass("pressed-button");;})
+      }
+    };
+  }])
 }).call(this);
