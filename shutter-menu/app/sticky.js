2d5359762a5abd328193edaf280b60cdd1ea8f18
nishant <nishant.singh87@gmail.com>
2015-09-27 01:00:09 +0530

    stick table filter to top.

--- shutter-menu/app/sticky.js
+++ shutter-menu/app/sticky.js
@@ -1,14 +1,19 @@
 (function () {
   "use strict"
   // shows when out of initial view port
-  module.directive("sticky", [function(){
+  module.directive("stickTo", [function(){
     return {
       restrict: 'A',
       transclude: false,
       scope: false,
       link: function(scope, element, attrs){
+        var target = $("#" + attrs.stickTo);
+        target.hide();
+        target.html("");
+        target.append($(element).clone());
+
         $(window).bind("scroll", function () {
-          this.pageYOffset > 100 ? element.show() : element.hide();
+          this.pageYOffset > 100 ? target.show() : target.hide();
         });
       }
     }
