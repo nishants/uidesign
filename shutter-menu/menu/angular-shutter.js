84f8fd424aa2faf1c9fbbe359e22ce6e48184140
nishant <nishant.singh87@gmail.com>
2015-09-26 18:01:33 +0530

    adding sticky header.

--- shutter-menu/menu/angular-shutter.js
+++ shutter-menu/menu/angular-shutter.js
@@ -138,4 +138,45 @@
       }
     };
   }])
+
+  module.directive("stickyHeader", [function(){
+    var setHeaderSize = function(fromHeader, toHeader){
+      var $fromTheads = fromHeader.find("th"),
+          $toTheads = toHeader.find("th");
+
+      for(var i = 0; i< $fromTheads.length; i++){
+        $($toTheads[i]).width($($fromTheads[i]).width());
+      }
+    };
+
+    return {
+      restrict: 'A',
+      transclude: false,
+      scope: false,
+      link: function(scope, element, attrs){
+        var $sticker = $(attrs.stickerSelector),
+            headerOffset = $(element).offset().top;
+
+        $sticker.hide();
+
+        console.log("will stick to " + $sticker.attr("id"));
+        $(window).bind("scroll", function () {
+          $sticker.html("");
+          var offset = $(this).scrollTop(),
+              $header = $(element).clone(),
+              $stickyeader = $($sticker).append($header);
+
+
+          setHeaderSize($(element), $header);
+          if (offset >= headerOffset && $sticker.is(":hidden")) {
+            $sticker.show();
+          }
+          else if (offset < headerOffset) {
+            $stickyeader.hide();
+          }
+        });
+
+      }
+    };
+  }])
 }).call(this);
