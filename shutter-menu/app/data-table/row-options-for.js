64c4c8c1881d5340a5d8addf822716dd3c90ba4d
nishant <nishant.singh87@gmail.com>
2015-09-27 15:14:37 +0530

    show row options in focused mode.

--- shutter-menu/app/data-table/row-options-for.js
+++ shutter-menu/app/data-table/row-options-for.js
@@ -1,21 +1,20 @@
 (function () {
   "use strict"
 
-  module.directive("rowOptionsFor", ["RowOption", function(RowOption){
+  module.directive("rowOptionsFor", ["RowOption", function (RowOption) {
 
     return {
       restrict: 'A',
       transclude: false,
       scope: false,
-      link: function(scope, element, attrs){
+      link: function (scope, element, attrs) {
         var $e = $(element);
 
-        $e.on("click", function(){
-          scope.$apply(function(){
+        $e.on("click", function () {
+          scope.$apply(function () {
             scope.dataTable.showOptionsFor(RowOption.new(scope.$eval(attrs.rowOptionsFor), $e));
           });
         });
-
       }
     }
   }]);
