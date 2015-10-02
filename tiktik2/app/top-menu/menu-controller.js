cc28a38c8bceb5edf004c8fb530729a237833b81
nishant <nishant.singh87@gmail.com>
2015-10-02 23:06:40 +0530

    show tabs fo rhovered sections.

--- tiktik2/app/top-menu/menu-controller.js
+++ tiktik2/app/top-menu/menu-controller.js
@@ -4,9 +4,27 @@
   angular.module("tiktik").controller("TopMenuController", ["$scope", function($scope){
 
     $scope.menu = {
-      section:  function(name){return name == "campaigns";},
-      tab: function(name){return name == "campaigns";},
-      showMenuFor: function(section){console.log("showing menu for : " + section)},
+      sectionByUrl : "campaigns",
+      sectionByHover : null,
+
+      section: function (name) {
+        var userIsHovering = !!this.sectionByHover,
+            isHovered = this.sectionByHover == name,
+            isByUrl = this.sectionByUrl == name;
+
+        return isHovered || (!userIsHovering && isByUrl);
+      },
+
+      tab: function (name) {
+        return !this.sectionByHover && name == "campaigns";
+      },
+
+      showByUrl: function () {
+        this.sectionByHover = null;
+      },
+      showMenuFor: function (name) {
+        this.sectionByHover = name;
+      },
       showHoveredOptions: function(){console.log("show hovering options")},
     };
 
