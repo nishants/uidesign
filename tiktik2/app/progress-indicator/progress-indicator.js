46f6bdac12a2d31cdaf050ac3b5843a8a02ab171
nishant <nishant.singh87@gmail.com>
2015-10-06 16:39:48 +0530

    show states by scope model.

--- tiktik2/app/progress-indicator/progress-indicator.js
+++ tiktik2/app/progress-indicator/progress-indicator.js
@@ -3,31 +3,24 @@
   // shows when out of initial view port
   angular.module("tiktik").controller("ProgressIndicator", ['$scope', function ($scope) {
 
-    var progressIndicator = null;
-    if (!$scope.progressIndicator) {
-      console.error("please define progress bar in parent scope : $scope.progressIndicator = {states: [{name: 'name', label : 'label', current: true}]}");
-      progressIndicator = {
-        states: [
-          {
-            name: 'first-thing',
-            label: 'first thing',
-            description: 'do the first thing, so that you can do second',
-            summary: 'you did the first thing, you entered : i want food, i like mexican',
-          }, {
-            name: 'second-thing',
-            label: 'second thing',
-            description: 'do the second thing now',
-            summary: null,  // set by parent controller, when field is done
+    var progressIndicator = {
+      states: [
+        {
+          name: 'first-thing',
+          label: 'first thing',
+          description: 'do the first thing, so that you can do second',
+          summary: 'you did the first thing, you entered : i want food, i like mexican',
+        }, {
+          name: 'second-thing',
+          label: 'second thing',
+          description: 'do the second thing now',
+          summary: null,  // set by parent controller, when field is done
 
-            current: true
-          }]
-      };
-    }
+          current: true
+        }]
+    };
 
-    if(!progressIndicator){
-      $scope.progressIndicator = progressIndicator;
-    }
-
+    $scope.progressIndicator = progressIndicator;
 
   }]);
 
