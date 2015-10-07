9824a6c6150c1e63b11dbde9036f62c052e301c4
nishant <nishant.singh87@gmail.com>
2015-10-07 09:28:19 +0530

    show step numbers for progress states.

--- tiktik2/app/progress-indicator/progress-indicator.js
+++ tiktik2/app/progress-indicator/progress-indicator.js
@@ -7,19 +7,19 @@
       states: [
         {
           name: 'create-adset',
-          label: '1. create adset',
+          label: 'create adset',
           description: 'do the first thing, so that you can do second',
           summary: 'you did the first thing, you entered : i want food, i like mexican',
         }, {
           name: 'set-bid-parameters',
-          label: '2. set bid parameters',
+          label: 'set bid parameters',
           description: 'do the second thing now',
           summary: null,  // set by parent controller, when field is done
 
           current: true
         }, {
           name: 'confirm',
-          label: '3. confirm',
+          label: 'confirm',
           description: 'do the second thing now',
           summary: null,  // set by parent controller, when field is done
         }]
