cc01b43153d357c1dab10a76074e50f2257193b7
nishant <nishant.singh87@gmail.com>
2015-10-07 09:06:07 +0530

    making progress indicator align centrally.

--- tiktik2/app/progress-indicator/progress-indicator.js
+++ tiktik2/app/progress-indicator/progress-indicator.js
@@ -6,17 +6,22 @@
     var progressIndicator = {
       states: [
         {
-          name: 'first-thing',
-          label: 'first thing',
+          name: 'create-adset',
+          label: '1. create adset',
           description: 'do the first thing, so that you can do second',
           summary: 'you did the first thing, you entered : i want food, i like mexican',
         }, {
-          name: 'second-thing',
-          label: 'second thing',
+          name: 'set-bid-parameters',
+          label: '2. set bid parameters',
           description: 'do the second thing now',
           summary: null,  // set by parent controller, when field is done
 
           current: true
+        }, {
+          name: 'confirm',
+          label: '3. confirm',
+          description: 'do the second thing now',
+          summary: null,  // set by parent controller, when field is done
         }]
     };
 
