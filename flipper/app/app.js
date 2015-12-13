24cf7e7616104fadaba97d82ea2214daced9b595
nishant <nishant.singh87@gmail.com>
2015-12-14 04:10:23 +0530

    renaming app.loaded to app.ready.

--- flipper/app/app.js
+++ flipper/app/app.js
@@ -2,12 +2,7 @@
   "use strict"
   window.Flipper.app = angular.module("flipper", ["ui.router"]);
   window.Flipper.app.controller("AppController", function($rootScope, $timeout){
-    var app         = {loaded: true},
-        hideLoader  = function(){app.loaded = true;};
-
-    $timeout(hideLoader, 5000);
-
-    $rootScope.app = app;
+    $rootScope.app = {ready: true};
   })
 
 }).call(this);
