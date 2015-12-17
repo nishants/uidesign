f86486c146e8b332930444ae5259d36363e0d4cd
nishant <nishant.singh87@gmail.com>
2015-12-18 05:27:52 +0530

    store compressed temlpates into local storage.

--- true-client-app/ng-client-app.js
+++ true-client-app/ng-client-app.js
@@ -1,14 +1,22 @@
 (function () {
   "use strict"
-  var app = angular.module("ngClientApp", []);
-  app.service("ngClientApp", function(){
+  var app = angular.module("ngClientApp", ["LocalStorageModule"]);
+
+  app.config(function (localStorageServiceProvider) {
+    localStorageServiceProvider
+        .setPrefix('ngClientApp');
+  });
+
+  app.service("ngClientApp", function($log, localStorageService){
+    if(!localStorageService.isSupported) {
+      $log.error("LocalStorageModule not found.");
+    }
     return {
-      html: null,
       load: function(){
-        return LZString.decompressFromUTF16(this.html) ;
+        return LZString.decompressFromUTF16(localStorageService.get("client-data")) ;
       },
       update: function(html){
-        this.html = LZString.compressToUTF16(html);;
+        localStorageService.set("client-data", LZString.compressToUTF16(html))
       }
     };
   });
--- true-client-app/readme.md
+++ true-client-app/readme.md
@@ -6,6 +6,8 @@ This will allow a seamless user experience at the cost of size of local storage 
 Tech stack :
 Version 1  : Prototype
           - lz-string : for compressions (results 444ms to compress 82 kb of sample-storage-string/01-js-css-html.html to 42 kb, and 42ms to decompress)
-
            https://github.com/pieroxy/lz-string
+
+           - angular local storage : for using local storage in angular's way.
+           https://github.com/grevory/angular-local-storage
 
