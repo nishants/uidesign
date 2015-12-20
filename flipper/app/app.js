1dda7a6d5b123712a9231522e65d74d400e71a2d
nishant <nishant.singh87@gmail.com>
2015-12-21 02:41:42 +0530

    load dresses, before loading the dresses.

--- flipper/app/app.js
+++ flipper/app/app.js
@@ -1,8 +1,12 @@
 (function () {
   "use strict"
   window.Flipper.app = angular.module("flipper", ["ui.router"]);
-  window.Flipper.app.controller("AppController", function($rootScope, $timeout){
-    $rootScope.app = {ready: true};
+
+  window.Flipper.app.controller("AppController", function($rootScope, $scope, $http){
+    $http.get("contract/dresses.json").then(function(response){
+      $rootScope.app = {ready: true};
+      $scope.home = {dresses: response.data.items};
+    })
   })
 
 }).call(this);
