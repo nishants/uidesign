b94268e6050deb9869ec911145236b58a85bb0b9
nishant <nishant.singh87@gmail.com>
2015-12-22 05:53:10 +0530

    route for homepage.

--- flipper/app/routes.js
+++ flipper/app/routes.js
@@ -2,14 +2,10 @@
   "use strict"
 
   Flipper.app.config(function ($stateProvider) {
-     Flipper.pages.forEach(function(page){
-       $stateProvider
-           .state(page.name, {
-             url: page.url,
-             templateUrl:page.html,
-             controller: page.controller
-           });
-     });
+    $stateProvider.state("home", {
+          url: "",
+          templateUrl:"app/dress/home.html",
+        });
   });
 
 }).call(this);
