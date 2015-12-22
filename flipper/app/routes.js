863d97296de1bacdabcc59b2704ebb126e1b848d
nishant <nishant.singh87@gmail.com>
2015-12-22 06:09:02 +0530

    sample route.

--- flipper/app/routes.js
+++ flipper/app/routes.js
@@ -1,11 +1,14 @@
 (function () {
   "use strict"
 
-  Flipper.app.config(function ($stateProvider) {
+  Flipper.app.config(["$stateProvider", function ($stateProvider) {
     $stateProvider.state("home", {
-          url: "",
-          templateUrl:"app/dress/home.html",
-        });
-  });
+      url: "/",
+      templateUrl:"app/dress/home.html",
+    }).state("dress", {
+      url: "/dress",
+      template:"<h1>transitioned ? </h1>",
+    });
+  }]);
 
 }).call(this);
