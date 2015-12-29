078bf5e4dd3432a879a5a0cb2422951e5d525af8
nishant <nishant.singh87@gmail.com>
2015-12-29 23:34:30 +0530

    update collection when url changes.

--- concepts/mobile-shutter-menu/app/app.js
+++ concepts/mobile-shutter-menu/app/app.js
@@ -2,22 +2,26 @@
   "use strict"
   var app = angular.module("app", []);
 
-  app.controller("AppController", ["$scope", "Collections", function($scope, Collections){
+  app.controller("AppController", ["$scope", "Collections", "$location", function($scope, Collections, $location){
     $scope.app = {
-      collection: {name: "home", items: []}
+      collection: {
+        name: function(){
+          return $location.url().split("collection/")[1];
+        },
+        items: []
+      }
     };
 
     $scope.nav = {showSearch: false, showMenu: false};
     var updateCollection = function(){
       $scope.app.updatingCollection = true;
-      Collections.fetch($scope.app.collection.name).then(function(collection){
+      Collections.fetch($scope.app.collection.name()).then(function(collection){
         $scope.app.collection.items = collection;
         $scope.app.updatingCollection = false;
       });
     };
 
-    $scope.$watch("app.collection.name", updateCollection);
-
+    $scope.$watch("app.collection.name()", updateCollection);
   }]);
 
   app.service("Collections", ["$http", function($http){
@@ -30,5 +34,4 @@
       }
     }
   }]);
-
 })();
