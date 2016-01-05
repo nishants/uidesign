62880126fe731ff1a9fa8020a69f892b2c730758
nishant <nishant.singh87@gmail.com>
2016-01-06 02:01:33 +0530

    allow adding only four missions.

--- lessons/star-wars/app/game-controller.js
+++ lessons/star-wars/app/game-controller.js
@@ -13,11 +13,21 @@
         this.expandBottomBar = false;
       },
       selectSpacecraftFor: function(planet){
+        if(this.noMoreMissions()) return;
         this.selectedPlanet = planet;
         this.expandBottomBar = true;
       },
       toggleSpacecraftMenu: function(){
         this.expandBottomBar = !this.expandBottomBar;
+      },
+      noMoreMissions : function(){
+        var planets = this.planets();
+        var missions = 0;
+        for(var i =0; i< planets.length; i++){
+          var planet = planets[i];
+          missions += planet.spacecraft ? 1 : 0;
+        }
+        return missions == 4;
       }
     };
   }])
