cac9ad2a8daea5b8fd88a00ea6b62dcf1c4241df
nishant <nishant.singh87@gmail.com>
2016-01-06 03:53:41 +0530

    show images for planets.

---
+++ lessons/star-wars/app/planet-image-filter.js
@@ -0,0 +1,19 @@
+(function () {
+  "use strict"
+
+  game.filter("imageFor", function(){
+    var images = {
+      "Donlon"  : "images/Donlon.png",
+      "Enchai"  : "images/Enchai.png",
+      "Jebing"  : "images/Jebing.png",
+      "Sapir"   : "images/Sapir.png",
+      "Lerbin"  : "images/Lerbin.jpg",
+      "Pingasor": "images/Pingasor.jpeg",
+      "other"   : "images/other-planet.jpeg"
+    };
+    return function(planet){
+      return images[planet.name] ? images[planet.name] : images["other"];
+    };
+  });
+
+}).call(this);
