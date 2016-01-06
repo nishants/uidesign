b81209f34e9bb1d62d5d68439a5cbbcd18e37702
nishant <nishant.singh87@gmail.com>
2016-01-07 01:43:25 +0530

    changin icon for Jebing planet.

--- lessons/star-wars/app/components/planet-icon.js
+++ lessons/star-wars/app/components/planet-icon.js
@@ -5,11 +5,11 @@
     var images = {
       "Donlon"  : "images/Donlon.png",
       "Enchai"  : "images/Enchai.jpg",
-      "Jebing"  : "images/Jebing.png",
+      "Jebing"  : "images/Jebing.jpeg",
       "Sapir"   : "images/Sapir.png",
       "Lerbin"  : "images/Lerbin.jpg",
       "Pingasor": "images/Pingasor.jpeg",
-      "other"   : "images/other-planet.jpeg"
+      "other"   : "images/other-planet.png"
     };
     return function(planet){
       return images[planet.name] ? images[planet.name] : images["other"];
