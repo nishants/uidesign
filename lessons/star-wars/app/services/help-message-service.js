afa045ac63c38d12f4579c708010c4414211b7fe
nishant <nishant.singh87@gmail.com>
2016-01-07 18:43:10 +0530

    formatting.

--- lessons/star-wars/app/services/help-message-service.js
+++ lessons/star-wars/app/services/help-message-service.js
@@ -3,56 +3,56 @@
   game.service("HelpMessageService", function () {
     var messages = [
       {
-        name          : "onLoad",
-        messageFor    : function(state){
+        name: "onLoad",
+        messageFor: function (state) {
           return "Create 4 missions.";
         },
-        appliesTo     : function(state){
+        appliesTo: function (state) {
           return !state.expandBottomBar && !state.selectedPlanet && state.assignedMissons() == 0;
         },
       },
       {
-        name          : "showVehicles",
-        messageFor    : function(){
+        name: "showVehicles",
+        messageFor: function () {
           return "Select planet and assign vehicle.";
         },
-        appliesTo     : function(state){
+        appliesTo: function (state) {
           return state.expandBottomBar && !state.selectedPlanet && !state.noMoreMissions();
         },
       },
       {
-        name          : "selectVehicle",
-        messageFor    : function(state){
+        name: "selectVehicle",
+        messageFor: function (state) {
           return "Select a spacecraft with range over " + state.selectedPlanet.distance + "megamiles.";
         },
-        appliesTo     : function(state){
-          return state.expandBottomBar && state.selectedPlanet ;
+        appliesTo: function (state) {
+          return state.expandBottomBar && state.selectedPlanet;
         },
       },
       {
-        name          : "selectMore",
-        messageFor    : function(state){
+        name: "selectMore",
+        messageFor: function (state) {
           return "Create " + (4 - state.assignedMissons()) + " more missions.";
         },
-        appliesTo     : function(state){
-          return !state.expandBottomBar && !state.selectedPlanet && !state.noMoreMissions() ;
+        appliesTo: function (state) {
+          return !state.expandBottomBar && !state.selectedPlanet && !state.noMoreMissions();
         },
       },
       {
-        name          : "submit",
-        messageFor    : function(state){
+        name: "submit",
+        messageFor: function (state) {
           return "Click on Find Falcone. Good Luck !";
         },
-        appliesTo     : function(state){
-          return state.noMoreMissions() ;
+        appliesTo: function (state) {
+          return state.noMoreMissions();
         },
       }
     ];
 
     return {
-      messageFor: function(state){
-        for(var i = 0; i < messages.length; i++){
-          if(messages[i].appliesTo(state)){
+      messageFor: function (state) {
+        for (var i = 0; i < messages.length; i++) {
+          if (messages[i].appliesTo(state)) {
             return messages[i].messageFor(state);
           }
         }
