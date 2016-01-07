6d62b6a3a168bdf4384dff889689e2d102853dc5
nishant <nishant.singh87@gmail.com>
2016-01-07 18:36:38 +0530

    calculate assigned missions.

--- lessons/star-wars/app/controllers/game-controller.js
+++ lessons/star-wars/app/controllers/game-controller.js
@@ -39,14 +39,17 @@
       toggleSpacecraftMenu: function(){
         this.expandBottomBar = !this.expandBottomBar;
       },
-      noMoreMissions : function(){
+      assignedMissons : function(){
         var planets = this.planets();
         var missions = 0;
         for(var i =0; i< planets.length; i++){
           var planet = planets[i];
           missions += planet.spacecraft ? 1 : 0;
         }
-        return missions == 4;
+        return missions;
+      },
+      noMoreMissions : function(){
+        return this.assignedMissons() == 4;
       },
       removeMissionFor: function(planet){
         planet.spacecraft.count ++;
