99ecd41a5c82b748a175fdb03bac4c364a11c676
nishant <nishant.singh87@gmail.com>
2016-01-08 14:30:39 +0530

    moving planet icons to model.

--- lessons/star-wars/app/missions/planets.js
+++ lessons/star-wars/app/missions/planets.js
@@ -1,10 +1,25 @@
 (function () {
   "use strict"
   game.factory("Planets", function () {
+    var icons = {
+      "Donlon"  : "images/Donlon.png",
+      "Enchai"  : "images/Enchai.jpg",
+      "Jebing"  : "images/Jebing.jpeg",
+      "Sapir"   : "images/Sapir.png",
+      "Lerbin"  : "images/Lerbin.jpg",
+      "Pingasor": "images/Pingasor.jpeg",
+      "other"   : "images/other-planet.png"
+    };
+
+    var iconFor = function(planet){
+      return icons[planet.name] ? icons[planet.name] : icons["other"];
+    }
+
     var Planet = function (data) {
       this.name = data.name;
       this.distance = data.distance;
       this.spacecraft = null;
+      this.icon = iconFor(data);
     };
 
     return {
