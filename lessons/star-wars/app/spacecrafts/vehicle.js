9b1c319d148608a6528583bcd0105874d4460f8e
nishant <nishant.singh87@gmail.com>
2016-01-08 14:33:44 +0530

    moving vehicle icons to model.

--- lessons/star-wars/app/spacecrafts/vehicle.js
+++ lessons/star-wars/app/spacecrafts/vehicle.js
@@ -1,11 +1,24 @@
 (function () {
   "use strict"
   game.factory("Vehicles", function () {
+    var icons = {
+      "Space pod"     : "images/space-pod.jpg",
+      "Space rocket"  : "images/space-rocket.png",
+      "Space shuttle" : "images/shutle.jpeg",
+      "Space ship"    : "images/spaceship.png",
+      "other"         : "images/other-vehicle.jpg"
+    };
+
+    var iconFor = function (vehicle) {
+      return icons[vehicle.name] ? icons[vehicle.name] : icons["other"];
+    }
+
     var Vehicle = function (data) {
-      this.name = data.name;
+      this.name  = data.name;
       this.count = data.total_no;
       this.range = data.max_distance;
       this.speed = data.speed;
+      this.icon  = iconFor(data);
     };
 
     return {
