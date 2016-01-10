1849ded684e6fba76b4bdfef91cdaf3dacca7ce4
nishant <nishant.singh87@gmail.com>
2016-01-10 19:15:26 +0530

    adding fade-in effect for mission items.

---
+++ lab/galaxy/app/diretictives/fade-in.js
@@ -0,0 +1,16 @@
+(function () {
+  "use strict"
+  galaxy.directive("fadeIn", function(){
+    return {
+      restrict: "C",
+      scope: false,
+      transclude: false,
+      link: function(scope, element){
+        setTimeout(function(){
+          element.addClass("show");
+        },100);
+      }
+    };
+  });
+
+}).call(this);
