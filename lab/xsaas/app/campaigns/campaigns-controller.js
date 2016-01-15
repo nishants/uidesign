b79e6a6fb91069428737c2651d95e331697246a3
nishant <nishant.singh87@gmail.com>
2016-01-15 13:30:06 +0530

    extracting power access vars in scope.

--- lab/xsaas/app/campaigns/campaigns-controller.js
+++ lab/xsaas/app/campaigns/campaigns-controller.js
@@ -1,6 +1,14 @@
 (function(){
   "use strict"
   app.controller("CampaignsController", function($scope, Campaigns){
+    $scope.ui = {
+      section  : 'show-campaigns',
+      showMenu :  '',
+      showCampaigns: function(){
+        this.showMenu == 'show-campaigns-filter' ?this.showMenu = null : this.showMenu = 'show-campaigns-filter';
+      }
+    };
+
     $scope.Campaigns = {
       data : [],
     };
