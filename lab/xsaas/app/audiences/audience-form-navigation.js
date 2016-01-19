b0cd79e408af36be892099ccdb25085ae4d646bd
nishant <nishant.singh87@gmail.com>
2016-01-19 23:59:59 +0530

    fixing scroll to target.

--- lab/xsaas/app/audiences/audience-form-navigation.js
+++ lab/xsaas/app/audiences/audience-form-navigation.js
@@ -6,30 +6,14 @@
       restrict: "C",
       scope: false,
       transclude: false,
-      link: function(scope, element, attrs){
-
-        var offsets;
-        var calculateOffset = function(element){
-          var offset = 0,
-              sections = element.find(".form-section");
-          offsets = {};
-          for(var i =0; i < sections.length; i++){
-            var section  = $(sections[i]);
-            offset = section.offset().top;
-            offsets[section.attr("id")] = offset;
-          }
-          window.offsets = offsets;
-
-        };
+      link: function(scope, element){
         var scrollable = $(element).find(".scrollable").first();
         scope.goto = function(targetId){
-          scrollable.animate({scrollTop: 0}, 10, function(){
-            var target = scrollable.find("#" + targetId);
-            var scrollTop = target.offset().top - target.height();
-            scrollable.animate({scrollTop: scrollTop}, 500);
-          });
+          var target = scrollable.find("#" + targetId);
+          var scrollTop = scrollable.scrollTop() ? target.offset().top - target.height() + scrollable.scrollTop() : target.offset().top - target.height();
+          console.log("scrollable.scrollTop(): "+scrollable.scrollTop() +", target.offset().top: "+ target.offset().top);
+          scrollable.animate({scrollTop: scrollTop}, 500);
         }
-
       }
     };
   });
