d00ad06acca4c19e87e16aeba3a858a83129b74a
nishant <nishant.singh87@gmail.com>
2016-01-20 01:56:48 +0530

    set focus on form section value, on scrolling to.

--- lab/xsaas/app/audiences/audience.scrollable.js
+++ lab/xsaas/app/audiences/audience.scrollable.js
@@ -9,10 +9,17 @@
       link: function(scope, element){
         var scrollable = $(element).find(".scrollable").first(),
             $targetFor = function (targetId) {return scrollable.find("#" + targetId);},
-            offsetOf = function(target){return target.offset().top - target.height() + scrollable.scrollTop();};
+            offsetOf = function(target){return target.offset().top - target.height() + scrollable.scrollTop();},
+            setFocus    = function($e){
+              $e.removeClass("flash-focus");
+              setTimeout(function(){
+                $e.addClass("flash-focus");
+              },0)
+            };
 
         scope.goto = function(targetId){
           scrollable.animate({scrollTop: offsetOf($targetFor(targetId))}, 500);
+          setFocus($targetFor(targetId));
         }
       }
     };
