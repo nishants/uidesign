8ca241b9a23fe8e7fb2f0e24f0087768cb2180e5
nishant <nishant.singh87@gmail.com>
2016-01-20 03:24:51 +0530

    adding section controller.

---
+++ lab/xsaas/app/audiences/audience-section-controller.js
@@ -0,0 +1,9 @@
+(function () {
+  "use strict"
+  app.controller("AudienceSectionController", function ($scope) {
+    $scope.state = {
+      showTable: false,
+      showForm: false,
+    };
+  });
+}).call(this);
