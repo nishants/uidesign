3dffffd243e8e22bdbe6f7a5a1bc55c96c50425d
nishant <nishant.singh87@gmail.com>
2016-01-29 23:20:11 +0530

    refactoring.

--- lab/serializable-state/test-serializer.js
+++ lab/serializable-state/test-serializer.js
@@ -1,12 +1,14 @@
 describe("serialize fields", function(){
-  var definition = {fields: [{name: "name"}, {name: "created", type: "date"}, {name: "alwaysMissing", type: "date"}]};
-
+  var definition        = {fields: [{name: "name"}, {name: "created", type: "date"}, {name: "alwaysMissing", type: "date"}]},
+      nameValue         = "some Name string 129012 /-=7#$%@#$%&^*&*()",
+      expectedNameHash  ="name="+encodeURIComponent(nameValue),
+      onDate            = new Date(),
+      onDateHash        = "created=" + onDate.getTime(),
+      serializer        = new ox.define(definition);
 
   it("should serialize string fields, if field type not set", function(){
-    var expected = "name=" + encodeURIComponent("First Last"),
-        encoded    = new ox.define(definition).encode({name: "First Last"});
-
-    expect(encoded).toEqual(expected);
+    var encoded    = serializer.encode({name: nameValue});
+    expect(encoded).toEqual(expectedNameHash);
   });
 
   it("should serialize date fields", function(){
@@ -19,7 +21,6 @@ describe("serialize fields", function(){
 
   it("should serialize all present fields in params", function(){
     var nameValue = "I am X.",
-        onDate     = new Date(),
         expected   = "name=" + encodeURIComponent(nameValue)+ "&" + "created=" + onDate.getTime(),
         encoded    = new ox.define(definition).encode({name: nameValue, created: onDate});
 
