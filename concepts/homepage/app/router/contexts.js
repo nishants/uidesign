76427aedc6e6066c338c563e5ff12e04fe91eaeb
nishant <nishant.singh87@gmail.com>
2016-01-31 13:22:05 +0530

    adding contexts.switch to .

--- concepts/homepage/app/router/contexts.js
+++ concepts/homepage/app/router/contexts.js
@@ -28,7 +28,20 @@
     return {
       forName : function(routeName){
         return contexts[routeName];
-      }
+      },
+      switchTo : function(viewIndex, stateIndex){
+      var views       = $(".views").first(),
+          states      = $(".view" + viewIndex+" > .states").first(),
+
+          viewHeight  = $("[layout]").first().height(),
+          viewWidth   = $("[layout]").first().width(),
+
+          offsetView  = viewIndex * viewWidth,
+          offsetState = stateIndex * viewHeight;
+
+      views.css("transform", "translateX(-" +offsetView +"px)");
+      states.css("transform", "translateY(-"+offsetState + "px)");
+    }
     };
   });
 }).call(this);
--- concepts/homepage/app/router/router.js
+++ concepts/homepage/app/router/router.js
@@ -2,20 +2,6 @@
   "use strict"
   app.service("router",function(Contexts){
 
-    var setView = function(viewIndex, stateIndex){
-      var views       = $(".views").first(),
-          states      = $(".view" + viewIndex+" > .states").first(),
-
-          viewHeight  = $("[layout]").first().height(),
-          viewWidth   = $("[layout]").first().width(),
-
-          offsetView  = viewIndex * viewWidth,
-          offsetState = stateIndex * viewHeight;
-
-      views.css("transform", "translateX(-" +offsetView +"px)");
-      states.css("transform", "translateY(-"+offsetState + "px)");
-    };
-
     var currentView = null,currentState = null;;
     return {
       param: {id: null},
@@ -25,7 +11,7 @@
         currentState = currentView.stateByName(tokens[3]);
       },
       render : function(){
-        setView(currentView.index, currentState ? currentState.index : 0);
+        Contexts.switchTo(currentView.index, currentState ? currentState.index : 0);
       }
     };
   });
