4346c78afd872af802d57a26bb5d604ea25bd8bf
nishant <nishant.singh87@gmail.com>
2016-01-31 13:19:43 +0530

    removing router.view and state.

--- concepts/homepage/app/router/router.js
+++ concepts/homepage/app/router/router.js
@@ -2,9 +2,6 @@
   "use strict"
   app.service("router",function(Contexts){
 
-    var defaultView   = null,
-        defaultState  = null;
-
     var setView = function(viewIndex, stateIndex){
       var views       = $(".views").first(),
           states      = $(".view" + viewIndex+" > .states").first(),
@@ -18,17 +15,17 @@
       views.css("transform", "translateX(-" +offsetView +"px)");
       states.css("transform", "translateY(-"+offsetState + "px)");
     };
+
+    var currentView = null,currentState = null;;
     return {
-      view: defaultView,
-      state: defaultState,
       param: {id: null},
       load: function (url) {
         var tokens = url.split("/");
-        this.view  = Contexts.forName(tokens[1]);
-        this.state = this.view.stateByName(tokens[3]);
+        currentView  = Contexts.forName(tokens[1]);
+        currentState = currentView.stateByName(tokens[3]);
       },
       render : function(){
-        setView(this.view.index, this.state ? this.state.index : 0);
+        setView(currentView.index, currentState ? currentState.index : 0);
       }
     };
   });
