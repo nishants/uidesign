7a42b8bcda7977183da4f0fb8299d191ae31e7db
nishant <nishant.singh87@gmail.com>
2016-01-31 22:45:17 +0530

    moving resize to layout service.

--- concepts/homepage/app/router/layout-service.js
+++ concepts/homepage/app/router/layout-service.js
@@ -1,21 +1,34 @@
 (function () {
   "use strict"
 
-  app.service("layout", function(){
+  app.service("layout", function(config){
+    var resize = function(){
+            var layout       = $(".views").first();
+            var viewHeight   = $("#app").height() - $("#top-bar").height();
+            var viewWidth    = $("#app").width();
+
+            layout.height( viewHeight);
+            layout.width(config.routes.length * viewWidth);
+
+            // resize the current view to app width
+            $(window).trigger("hashchange");
+          },
+        switchTo = function (viewIndex, stateIndex) {
+          var views = $(".views").first(),
+              states = $(".view" + viewIndex + " > .states").first(),
+
+              viewHeight = $("[layout]").first().height(),
+              viewWidth = $("[layout]").first().width(),
+
+              offsetView = viewIndex * viewWidth,
+              offsetState = stateIndex * viewHeight;
+
+          views.css("transform", "translateX(-" + offsetView + "px)");
+          states.css("transform", "translateY(-" + offsetState + "px)");
+        };
+
     return {
-      switchTo: function (viewIndex, stateIndex) {
-        var views = $(".views").first(),
-            states = $(".view" + viewIndex + " > .states").first(),
-
-            viewHeight = $("[layout]").first().height(),
-            viewWidth = $("[layout]").first().width(),
-
-            offsetView = viewIndex * viewWidth,
-            offsetState = stateIndex * viewHeight;
-
-        views.css("transform", "translateX(-" + offsetView + "px)");
-        states.css("transform", "translateY(-" + offsetState + "px)");
-      }
-    }
+      resize : resize,
+      switchTo: switchTo }
   });
 }).call(this);
--- concepts/homepage/app/router/layout.js
+++ concepts/homepage/app/router/layout.js
@@ -1,29 +1,19 @@
 (function () {
   "use strict"
 
-  app.directive("layout", function (config) {
-
-    var resize = function(){
-          var layout       = $(".views").first();
-          var viewHeight   = $("#app").height() - $("#top-bar").height();
-          var viewWidth    = $("#app").width();
-
-          layout.height( viewHeight);
-          layout.width(config.routes.length * viewWidth);
-
-          // resize the current view to app width
-          $(window).trigger("hashchange");
-        };
+  app.directive("layout", function (layout) {
 
     return {
       restrict: "A",
       scope: false,
       transclude: false,
       link: function(scope){
-        $(window).on("resize", resize);
-        scope.render = function(index){
-          resize();
+        var resize = function () {
+          layout.resize();
         };
+
+        $(window).on("resize", resize);
+        scope.render = resize;
       }
     };
   });
