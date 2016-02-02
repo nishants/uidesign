07f5708a5210f98e93f297d5693cd644b9b65343
nishant <nishant.singh87@gmail.com>
2016-02-02 23:37:17 +0530

    adding swiper to flipper app.

--- concepts/flipper/app/flipper.js
+++ concepts/flipper/app/flipper.js
@@ -1,5 +1,5 @@
 (function(){
   "use strict"
-  var app = angular.module("flipper",[]);
+  var app = angular.module("flipper",["swiper"]);
   window.app = app;
 }).call(this);
--- concepts/flipper/app/ui.js
+++ concepts/flipper/app/ui.js
@@ -1,8 +1,19 @@
-(function(){
+(function () {
   "use strict"
-  app.service("ui",function(){
-  return  {
-      ready : true,
+  app.service("ui", function () {
+    var initialMessage = "Swipe a card left, just like tinder",
+        swiping = "Swiping away card-",
+        swiped = "Swiped away card-";
+
+    return {
+      ready: true,
+      message: initialMessage,
+      whileSwipingOut: function (index) {
+        this.message = swiping + index;
+      },
+      onSwipeOut: function (index) {
+        this.message = swiped + index;
+      }
     };
   });
 }).call(this);
