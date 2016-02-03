7c210f12b00f12a3e210af82eedc40f3ef5cef7f
nishant <nishant.singh87@gmail.com>
2016-02-03 17:34:31 +0530

    extracting map.js

---
+++ lab/world-traveler/map.js
@@ -0,0 +1,41 @@
+(function(){
+  "use strict"
+  var Country = function(map, country){
+    var isoCode = country.code;
+    var selector = "." + isoCode + ".land",
+        coast = map.find(selector),
+        inland = coast.find(selector);
+
+    this.land =  inland.length ? inland : coast;
+    this.position = country.position;
+    var self = this;
+  };
+
+  Country.prototype.highlight = function(){
+    this.land.css("fill", "red");
+    this.land.children().css("fill", "red");//required of island countries like Philllipines
+  };
+
+  var Map = function($map, $wrapper){
+    var countries = {};
+    var self = this;
+    countriesToInitialize.forEach(function(country){
+      countries[country.code] = new Country($map, country);
+      countries[country.code].land.on("click", function(){
+        countries[country.code].highlight();
+        self.findCountry(country.code);
+      });
+    });
+    this.countries = countries;
+    this.$wrapper = $wrapper;
+  };
+
+  Map.prototype.findCountry = function(isoCode){
+    var country = this.countries[isoCode];
+    this.$wrapper.css("transform", country.position);
+    country.highlight();
+    return country;
+  };
+
+  window.Map = Map;
+}).call(this);
