fa20c167fe20417938a4edf57251525e8cb995b0
nishant <nishant.singh87@gmail.com>
2016-02-06 06:24:12 +0530

    google play window integerated.

--- concepts/kitkat/app/google-play/google-play.js
+++ concepts/kitkat/app/google-play/google-play.js
@@ -1,5 +1,50 @@
-(function(){
-  "use strict"
-  $(document).ready(function(){
+
+var swipedOut = function($wrapper){
+  $wrapper.addClass("swiped-out");
+}, threshold = 100;
+
+var swipingOut = function($e){
+  $e.addClass("swiping-out")
+};
+var notSwipingOut = function($e){
+  $e.removeClass("swiping-out")
+};
+var swipeEnds = function(wrapper, target){
+  target.css("transform", "" );// allow transfrom through css class to apply
+  wrapper.removeClass("swiping-out");
+};
+
+var touchedAt = function (e) {
+  return e.originalEvent.touches[0].pageX
+};
+
+var Wrapper = function($e){
+  this.$e = $e;
+  var wrapper = this;
+  var swipable = $e.find(".swipable").first();
+  $e.on('touchstart', function (e) {
+    wrapper.startAt = touchedAt(e);
   });
-}).call(this);
+
+  $e.on('touchmove', function (e) {
+    wrapper.swipedTo = touchedAt(e);
+    var swipeDistance = wrapper.startAt - wrapper.swipedTo;
+    swipable.css("transform", "translateX(-"+ swipeDistance + "px)" );
+    swipeDistance > threshold ? swipingOut(wrapper.$e) : notSwipingOut(wrapper.$e);
+  });
+
+  $e.on('touchend', function (e) {
+    var swipeDistance = wrapper.startAt - wrapper.swipedTo;
+    swipeDistance > threshold ? swipedOut(wrapper.$e) : "";
+    swipeEnds(wrapper.$e, $(swipable));
+  });
+};
+
+var Swiper = function($e){
+  $e.find("li").each(function(i, swipable){
+    new Wrapper($(swipable));
+  });
+};
+$(document).ready(function(){
+  new Swiper($(".swiper").first());
+});
