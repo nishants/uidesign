c29acb565295023f713eb8d01243016b5af8c1f1
nishant <nishant.singh87@gmail.com>
2016-02-06 07:32:15 +0530

    load empty hash on load.

--- concepts/kitkat/app/router.js
+++ concepts/kitkat/app/router.js
@@ -2,16 +2,21 @@
   "use strict"
   $(document).ready(function(){
     var states  = {
-      undefined: "",
-      "google-play" : "show-google-play"
-    };
-    var route = function () {
-      if (window.location.hash.length) {
-        var stateName = window.location.hash.split("/")[1];
-        app.reload(states[stateName]);
-      }
-    };
-    $(window).on("hashchange", route);
-    $(window).trigger("hashchange", route);
+        undefined: "",
+        "google-play" : "show-google-play"
+        },
+        loadUrl = function (url) {
+          var stateName = url.split("/")[1];
+          app.reload(states[stateName]);
+        },
+        init = function () {
+          window.location.href = window.location.href + "#/"
+        };
+
+    $(window).on("hashchange", function () {
+      window.location.hash.length ?  loadUrl(window.location.hash) : init();
+    });
+
+    $(window).trigger("hashchange");
   });
 }).call(this);
