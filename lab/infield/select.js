47809d80e776b3466bad465b56c9377b1d5b5990
nishant <nishant.singh87@gmail.com>
2016-02-11 02:27:31 +0530

    display value for select mask.

--- lab/infield/select.js
+++ lab/infield/select.js
@@ -4,13 +4,13 @@
   angular.module("select", []).directive("select", function(){
     return {
       restrict: 'C',
-      scope: false,
-      template: "<div class='select-cover' ng-transclude></div><div class='mask'> </div>",
+      scope: true,
+      template: "<div class='select-cover' ng-transclude></div><div class='mask'><div class='value' ng-bind='select.value'></div><div class='icon fa fa-chevron-down'></div>",
       transclude: true,
       link: function(scope, element){
-        var select = element;
+        var select = element.find("select");
         scope.$watch(select.attr("ng-model"), function(val){
-          val ? element.addClass("active") : element.removeClass("active");
+          scope.select = {value : val[element.attr("display")]};
         })
       }
     }
