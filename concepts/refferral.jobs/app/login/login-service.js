3c9033092e593c99e37a9029e7f907cd829f0750
nishant <nishant.singh87@gmail.com>
2016-02-27 16:07:15 +0530

    set current session in local storage.

--- concepts/refferral.jobs/app/login/login-service.js
+++ concepts/refferral.jobs/app/login/login-service.js
@@ -2,17 +2,21 @@
   "use strict"
 
   app.service("loginService", ["$q","$timeout", function($q, $timeout){
-    var loggedInUser = localStorage.user && JSON.parse(localStorage.user);
+    var loggedInUser = localStorage.user && JSON.parse(localStorage.user),
+        login = function(user){
+          loggedInUser      = user;
+          localStorage.user = JSON.stringify(user);
+        };
 
     return {
       login: function(email, password){
         return $q(function(resolve, error){
           var success = function(user){
-                loggedInUser = user;
+                login(user);
                 resolve(user);
               };
           $timeout(function(reponse){
-            reponse = {user: {name: "loggedIn"}}
+            reponse = {user: {name: email}}
             email ? success(reponse.user) : error();
           }, 100)
         });
