d5a0054b3ef6d8c8652f64b055e7526851a83f30
nishant <nishant.singh87@gmail.com>
2016-02-27 09:22:13 +0530

    display user name.

--- concepts/refferral.jobs/app/ui-controller.js
+++ concepts/refferral.jobs/app/ui-controller.js
@@ -8,7 +8,8 @@
       login: false,
       loadSession: function () {
         var ui = this,
-            sessionFound = function (emailId) {
+            sessionFound = function (user) {
+              ui.user  = user;
               ui.ready = true;
             },
             noSession = function () {};
