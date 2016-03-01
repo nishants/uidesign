145769d375e4858d0c42050b2f007c01e7e76935
nishant <nishant.singh87@gmail.com>
2016-03-01 09:29:54 +0530

    refactoring app.js.

--- concepts/refferral.jobs/app/app.js
+++ concepts/refferral.jobs/app/app.js
@@ -1,12 +1,11 @@
-(function(){
+(function () {
   "use strict"
 
   var remote = {
-    "localhost"    : "http://localhost:5000",
-    "amoeba.social": "http://referral-jobs.herokuapp.com",
-  };
-
-  var app = angular.module("referral.jobs", []);
+        "localhost": "http://localhost:5000",
+        "amoeba.social": "http://referral-jobs.herokuapp.com",
+      },
+      app = angular.module("referral.jobs", []);
 
   app.value("server", {address: remote[window.location.hostname]});
   window.app = app;
