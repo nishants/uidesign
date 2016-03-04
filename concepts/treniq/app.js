ed0483016bed6b9800a6b2e9b40c292778ae694f
nishant <nishant.singh87@gmail.com>
2016-03-04 13:59:54 +0530

    ripple effect for cards.

--- concepts/treniq/app.js
+++ concepts/treniq/app.js
@@ -5,4 +5,16 @@
     app:  angular.module("treniq", [])
   };
 
+  treniq.app.directive("card" , [function(){
+    return {
+      restrict: "C",
+      transclude: false,
+      scope: false,
+      link: function(scope, element){
+        Cards.bind($(element));
+      }
+    };
+  }
+  ]);
+
 }).call(this);
