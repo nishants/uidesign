46cbaadde95c91a0586e0635b1b4e0ca85767a5e
nishant <nishant.singh87@gmail.com>
2016-03-04 13:59:35 +0530

    adding cards.js

---
+++ lab/cards/cards.js
@@ -0,0 +1,33 @@
+(function () {
+  "use strict"
+
+  var bindCard = function ($card) {
+        var action = $card.find(".action"),
+            button = action.find("button"),
+            ripple = action.find(".ripple");
+
+        button.on("mousedown touchstart", function () {
+          ripple.addClass("burst");
+          setTimeout(function () {
+            ripple.removeClass("burst");
+          }, 500);
+        });
+
+      },
+      cardsFrom = function ($deck) {
+        return $deck.find(" > li > .card");
+      };
+
+
+  window.Cards = {
+    bind: function($card){
+      bindCard(this.$card);
+    },
+    read: function($deck){
+      $deck.find(" > li > .card").each(function(index, card){
+        bindCard($(card));
+      });
+    }
+  };
+
+}).call(this);
