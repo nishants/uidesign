dfa5cf3c10e4ae67a1db20b28ec7bbec25119f74
nishant <nishant.singh87@gmail.com>
2016-04-01 03:07:22 +0530

    hide loader when iframe loads inside the frame container.

--- concepts/amoeba.social/frame-container.js
+++ concepts/amoeba.social/frame-container.js
@@ -1,21 +1,28 @@
 (function () {
   "use strict"
 
-  angular.module("slate").directive("frameContainer", ["$timeout",function ($timeout) {
+  angular.module("slate").directive("frameContainer", ["$timeout", function ($timeout) {
     return {
       restrict: "C",
       scope: true,
       transclude: false,
       link: function (scope, element, attrs) {
         var frameContainer = {
-          ready: false,
-        };
+              ready: false,
+            },
+            $frameContainer = $(element);
+
         scope.frameContainer = frameContainer;
 
+        $frameContainer.on("DOMNodeInserted", function (e) {
+          $frameContainer.find("iframe").load(function () {
+            $timeout(function () {
+              frameContainer.ready = true;
+            }, 0);
+          })
+        });
+
         scope.$watch("deck.current", function (now, previous) {
-          $timeout(function(){
-            frameContainer.ready = true;
-          }, 3000);
           frameContainer.ready = frameContainer.ready && (now != -1);
         });
       }
