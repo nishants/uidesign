85848f013b856b39823cc7f4e7aa634c82720bed
nishant <nishant.singh87@gmail.com>
2016-03-31 16:31:57 +0530

    adding cards to deck.cards.

--- concepts/slate/app.js
+++ concepts/slate/app.js
@@ -21,7 +21,27 @@
       transclude: false,
       link: function (scope, element, attrs) {
         scope.deck = {
-          current: -1
+          current: -1,
+          cards: [
+            {id: 0},
+            {id: 1},
+            {id: 2},
+            {id: 3},
+            {id: 4},
+            {id: 5},
+            {id: 6},
+            {id: 7},
+            {id: 8},
+            {id: 9},
+            {id: 10},
+            {id: 11},
+            {id: 12},
+            {id: 13},
+            {id: 14},
+            {id: 15},
+            {id: 16},
+            {id: 17},
+          ]
         };
         scope.$watch("deck.current", function (now, previous) {
           now != -1 && select(now);
