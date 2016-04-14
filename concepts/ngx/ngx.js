ab955f769607428732f3407b2a4ab69fc5026be2
dawn <nishant.singh87@gmail.com>
2016-04-15 01:13:24 +0530

    correct indentation.

--- concepts/ngx/ngx.js
+++ concepts/ngx/ngx.js
@@ -1,71 +1,71 @@
-(function(){
-    "use strict"
+(function () {
+  "use strict"
 
-    var helper = {
-        setterFor : function (string) {
-            var capitalized = string.charAt(0).toUpperCase() + string.slice(1);
-            return "set" + capitalized;
-        }
-    };
-    var findScopesIn = function($target){
-        var elements = $target.find("[ngx-scope]"),
-            scopes  = [];
-        for(var i =0; i < elements.length; i ++){
-            var $e   = $(elements[i]),
-                name = $e.attr("ngx-scope");
+  var helper = {
+    setterFor: function (string) {
+      var capitalized = string.charAt(0).toUpperCase() + string.slice(1);
+      return "set" + capitalized;
+    }
+  };
+  var findScopesIn = function ($target) {
+    var elements = $target.find("[ngx-scope]"),
+        scopes = [];
+    for (var i = 0; i < elements.length; i++) {
+      var $e = $(elements[i]),
+          name = $e.attr("ngx-scope");
 
-            scopes.push({name: name, $target: $e});
-        }
-        return scopes;
-    }, findModelsIn = function(scope){
-        var elements = scope.$target.find("[ngx-model]"),
-            models  = [];
-        for(var i =0; i < elements.length; i ++){
-            var $e        = $(elements[i]),
-                name      = $e.attr("ngx-model").replace(scope.name + ".", "");
+      scopes.push({name: name, $target: $e});
+    }
+    return scopes;
+  }, findModelsIn = function (scope) {
+    var elements = scope.$target.find("[ngx-model]"),
+        models = [];
+    for (var i = 0; i < elements.length; i++) {
+      var $e = $(elements[i]),
+          name = $e.attr("ngx-model").replace(scope.name + ".", "");
 
-            models.push({name: name, $target: $e});
-        }
-        return models;
+      models.push({name: name, $target: $e});
+    }
+    return models;
 
 
-        return {name: $target.find("[ngx-model]"), $target: $target, models : {}};
-    };
+    return {name: $target.find("[ngx-model]"), $target: $target, models: {}};
+  };
 
-    var ngx = {
-        $modules: {},
-        parse: function($e){
-            this.$modules[$e.attr("ngx-app")]  = {$target: $e};
-        },
-        module: function(name, params){
-            var newModule = {
-                    scopes: {},
-                    scope: function (name, params) {
-                        this.scopes[name] = params[params.length - 1]();
-                    }
-                },
-                self = this;
-            if(params != undefined){
-                findScopesIn(self.$modules[name].$target).forEach(function(scope){
-                    findModelsIn(scope).forEach(function(model){
-                        model.$target.on("input", function(){
-                            var setterName = helper.setterFor(model.name);
-                            var ngxScope = newModule.scopes[scope.name];
-                            var setterFunction = ngxScope[setterName];
-                            setterFunction.call(ngxScope);
-                        });
-                        console.log(model.name);
-                    });
-                });
+  var ngx = {
+    $modules: {},
+    parse: function ($e) {
+      this.$modules[$e.attr("ngx-app")] = {$target: $e};
+    },
+    module: function (name, params) {
+      var newModule = {
+            scopes: {},
+            scope: function (name, params) {
+              this.scopes[name] = params[params.length - 1]();
             }
+          },
+          self = this;
+      if (params != undefined) {
+        findScopesIn(self.$modules[name].$target).forEach(function (scope) {
+          findModelsIn(scope).forEach(function (model) {
+            model.$target.on("input", function () {
+              var setterName = helper.setterFor(model.name);
+              var ngxScope = newModule.scopes[scope.name];
+              var setterFunction = ngxScope[setterName];
+              setterFunction.call(ngxScope);
+            });
+            console.log(model.name);
+          });
+        });
+      }
 
-            return newModule;
-        }
-    };
+      return newModule;
+    }
+  };
 
-    $(document).ready(function(){
-        var $target = $("[ngx-app]").first();
-        ngx.parse($target);
-    });
-    window.ngx = ngx;
+  $(document).ready(function () {
+    var $target = $("[ngx-app]").first();
+    ngx.parse($target);
+  });
+  window.ngx = ngx;
 }).call(this);
