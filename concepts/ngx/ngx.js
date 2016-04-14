668730d1369f9acd49e74a93ba15837ebba71a49
dawn <nishant.singh87@gmail.com>
2016-04-15 00:29:49 +0530

    find scopes in app.

--- concepts/ngx/ngx.js
+++ concepts/ngx/ngx.js
@@ -1,12 +1,32 @@
 (function(){
     "use strict"
+    var findScopesIn = function($target){
+        var elements = $target.find("[ngx-scope]"),
+            scopes  = [];
+        for(var i =0; i < elements.length; i ++){
+            var $e        = $(elements[i]),
+                modelName = $e.find("[ngx-scope]");
+
+            scopes.push({name: modelName, $target: $e});
+        }
+        return scopes;
+    }, findModels = function($target){
+        return {name: $target.find("[ngx-model]"), $target: $target};
+    };
+
     var ngx = {
-        targets: [],
+        $modules: {},
         parse: function($e){
-            this.targets.push({name: $e.attr("ngx-app")});
+            this.$modules[$e.attr("ngx-app")]  = $e;
         },
         module: function(name, params){
             var newModule = {scope: function () {}};
+            if(params != undefined){
+                findScopesIn(this.$modules[name]).forEach(function(){
+
+                })
+            }
+
             return newModule;
         }
     };
