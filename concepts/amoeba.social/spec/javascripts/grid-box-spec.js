79fc563e8b2f0ecb4a661bfa53494023a4e3d23e
dawn <nishant.singh87@gmail.com>
2016-04-22 00:25:38 +0530

    refactoring grid-box-spec

--- concepts/amoeba.social/spec/javascripts/grid-box-spec.js
+++ concepts/amoeba.social/spec/javascripts/grid-box-spec.js
@@ -1,29 +1,15 @@
 describe("GridBox", function(){
-  var fakeGridElement = function(){
-    return {
-      property: null,
-      value   : null,
-      css: function (property, value) {
-        this.property = property;
-        this.value    = value;
-      },
-      applied : function(){
-        return {property: this.property, value: this.value};
-      }
-    };
-  };
 
   it("should set and apply position", function(){
-    var
-        $gridBox  = fakeGridElement(),
+    var $gridBox  = {css: function(){}},
         gridBox   = new GridBox($gridBox);
 
+    spyOn($gridBox, "css");
+
     gridBox.setPosition(21, 45);
-    expect($gridBox.applied().property).toBeNull();
-    expect($gridBox.applied().value).toBeNull();
+    expect($gridBox.css).not.toHaveBeenCalled();
 
     gridBox.applyPosition();
-    expect($gridBox.applied().property).toEqual("transform");
-    expect($gridBox.applied().value).toBe("translateX(21) translateY(45)");
+    expect($gridBox.css).toHaveBeenCalledWith("transform", "translateX(21) translateY(45)");
   });
 });
