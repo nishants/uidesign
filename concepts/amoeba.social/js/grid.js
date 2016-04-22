0009e17455142676a6c6c465a4cc3232b5218c11
dawn <nishant.singh87@gmail.com>
2016-04-22 15:50:54 +0530

    grid calculations.

--- concepts/amoeba.social/js/grid.js
+++ concepts/amoeba.social/js/grid.js
@@ -6,10 +6,19 @@
     this.gridBoxes  = gridBoxes;
     this.colWidth   = colWidth;
     this.columns    = [];
+    this.height     = 0;
   };
 
   Grid.prototype.collect = function(){
-    this.columns.length = this.$container.width()/this.colWidth;
+    var columnCount = this.$container.width()/this.colWidth,
+        gridHeight  = 0,
+        nextColumn  = 0;
+
+    for(var i = 0; i < columnCount; i++){
+      this.columns[i] = {nexPosition: 0};
+    }
+
+    this.gridBoxes[0].setPosition(0,0);
   };
 
   window.Grid = Grid;
--- concepts/amoeba.social/spec/javascripts/grid-spec.js
+++ concepts/amoeba.social/spec/javascripts/grid-spec.js
@@ -1,28 +1,39 @@
 describe("Grid", function(){
-  var gridBoxMock = function(height){
+  var gridBoxMock = function(height, stateName){
         var $gridBox  = {
               height: function(){return height;},
-              css: function(){}
+              css: function(){},
+              hasClass: function(name){return name === stateName;}
             },
             gridBox = new GridBox($gridBox);
+        spyOn(gridBox, "setPosition");
+
+        return gridBox;
       },
+
       $containerOfWidth = function(width){
         return {
           width : function(){return width;}
         };
-      } ;
+      };
 
-  it("should set grid.dimensions", function(){
+  it("should calculate grid dimensions on grid.calculate", function(){
     var
         colWidth        = 30,
         containerWidth  = 90,
         expectedColumns = containerWidth/colWidth,
         expectedHeight  = Math.max(20 + 35, 25 + 25, 36 + 20 ),
         $container      = $containerOfWidth(containerWidth),
-        gridBoxes       = [gridBoxMock(20), gridBoxMock(25), gridBoxMock(36), gridBoxMock(35), gridBoxMock(25), gridBoxMock(20)],
+        gridBoxes       = [gridBoxMock(20), gridBoxMock(25), gridBoxMock(36),
+                           gridBoxMock(35), gridBoxMock(25), gridBoxMock(20)],
         grid            = new Grid($container, gridBoxes, colWidth);
 
     grid.collect();
-    expect(grid.columns.length).toBe(3);
+
+    expect(grid.columns.length).toBe(expectedColumns);
+
+    expect(gridBoxes[0].setPosition).toHaveBeenCalledWith(0,0);
+
+    //expect(grid.height).toBe(expectedHeight);
   });
 });
