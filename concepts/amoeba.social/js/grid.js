8db72a78ccad090b4e3ea2fbf6599c9cc6f2939e
dawn <nishant.singh87@gmail.com>
2016-04-22 15:27:54 +0530

    grid calculations.

--- concepts/amoeba.social/js/grid.js
+++ concepts/amoeba.social/js/grid.js
@@ -1,11 +1,16 @@
 (function () {
   "use strict"
 
-  var Grid = function(){
-    this.dimensions = null;
+  var Grid = function($container, gridBoxes, colWidth){
+    this.$container = $container;
+    this.gridBoxes  = gridBoxes;
+    this.colWidth   = colWidth;
+    this.columns    = [];
   };
+
   Grid.prototype.collect = function(){
-    this.dimensions = {}
+    this.columns.length = this.$container.width()/this.colWidth;
   };
+
   window.Grid = Grid;
 }).call(this);
--- concepts/amoeba.social/spec/javascripts/grid-spec.js
+++ concepts/amoeba.social/spec/javascripts/grid-spec.js
@@ -1,13 +1,28 @@
 describe("Grid", function(){
-  var gridBoxMock = function(){
-        var $gridBox  = {css: function(){}},
+  var gridBoxMock = function(height){
+        var $gridBox  = {
+              height: function(){return height;},
+              css: function(){}
+            },
             gridBox = new GridBox($gridBox);
       },
-      gridBoxes = [gridBoxMock(), gridBoxMock(), gridBoxMock(), gridBoxMock(), gridBoxMock(), gridBoxMock()];
+      $containerOfWidth = function(width){
+        return {
+          width : function(){return width;}
+        };
+      } ;
 
   it("should set grid.dimensions", function(){
-    var grid = new Grid(gridBoxes);
+    var
+        colWidth        = 30,
+        containerWidth  = 90,
+        expectedColumns = containerWidth/colWidth,
+        expectedHeight  = Math.max(20 + 35, 25 + 25, 36 + 20 ),
+        $container      = $containerOfWidth(containerWidth),
+        gridBoxes       = [gridBoxMock(20), gridBoxMock(25), gridBoxMock(36), gridBoxMock(35), gridBoxMock(25), gridBoxMock(20)],
+        grid            = new Grid($container, gridBoxes, colWidth);
+
     grid.collect();
-    expect(grid.dimensions).not.toBeNull();
+    expect(grid.columns.length).toBe(3);
   });
 });
