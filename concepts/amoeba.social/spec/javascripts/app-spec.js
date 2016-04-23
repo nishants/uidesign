73a0877025d56f1a874345a983b45295dfae5241
dawn <nishant.singh87@gmail.com>
2016-04-23 22:30:53 +0530

    refactoring app-spec.

--- concepts/amoeba.social/spec/javascripts/app-spec.js
+++ concepts/amoeba.social/spec/javascripts/app-spec.js
@@ -1,20 +1,26 @@
 describe("App", function(){
+  var stateName = "state-name",
+      $state  ,
+      states,
+      grid ;
 
-  it("Should load url and apply state", function(){
-    var $state  = $mock({classes: ["some-other-state"]}),
-        states  = {parse: function(){}},
-        grid    = {collect: function(){this.collected = true;}, arrange: function(){if(!this.collected) throw "should collect before arrange";}}
-        app     = new App($state, states, grid);
+  beforeEach(function(){
+    states = {parse: function(){}}
+    $state = $mock({classes: ["some-other-state"]});
+    grid   = {collect: function(){this.collected = true;}, arrange: function(){if(!this.collected) throw "should collect before arrange";}}
 
-    spyOn(states,     "parse").and.returnValue({name: "state-name"});
+    spyOn(states,     "parse").and.returnValue({name: stateName});
     spyOn(grid,    "collect").and.callThrough();
     spyOn(grid,    "arrange").and.callThrough();
+  });
+
+  it("Should load url and apply state", function(){
+    var app     = new App($state, states, grid);
 
     app.loadUrl("#/state-id");
 
     expect($state.hasClass("state-name")).toBeTruthy();
     expect($state.hasClass("some-other-state")).toBeFalsy();
-
     expect(grid.collect).toHaveBeenCalledWith("state-name");
     expect(grid.arrange).toHaveBeenCalled();
   });
