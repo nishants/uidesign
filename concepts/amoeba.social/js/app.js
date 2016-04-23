e607c1509de1bb0521afae83deeec2922b05cb18
dawn <nishant.singh87@gmail.com>
2016-04-23 14:16:14 +0530

    rearrange grid on applying state.

--- concepts/amoeba.social/js/app.js
+++ concepts/amoeba.social/js/app.js
@@ -1,13 +1,16 @@
 (function () {
   "use strict"
 
-  var App = function($state){
+  var App = function($state, grid){
     this.$state = $state;
+    this.grid  = grid;
   };
 
   App.prototype.apply = function(state){
     this.$state.removeClass();
     this.$state.addClass(state.name);
+    this.grid.collect(state.name);
+    this.grid.arrange();
   };
 
   window.App = App;
--- concepts/amoeba.social/spec/javascripts/app-spec.js
+++ concepts/amoeba.social/spec/javascripts/app-spec.js
@@ -3,13 +3,18 @@ describe("App", function(){
   it("Should apply state", function(){
     var $state  = {addClass: function(){ if(!this.removed) throw "should clear state";}, removeClass: function(){this.removed = true; }},
         states  = new States([{id: "state-id", name: "state-name"}, {id: "other-state-id", name: "other-state-name"}]),
-        app     = new App($state, states);
+        grid    = {collect: function(){this.collected = true;}, arrange: function(){if(!this.collected) throw "should collect before arrange";}}
+        app     = new App($state, grid);
 
     spyOn($state, "addClass").and.callThrough();
+    spyOn(grid,   "collect").and.callThrough();
+    spyOn(grid,   "arrange").and.callThrough();
 
     app.apply(states.parse("#/state-id"));
 
     expect($state.addClass).toHaveBeenCalledWith("state-name");
+    expect(grid.collect).toHaveBeenCalledWith("state-name");
+    expect(grid.arrange).toHaveBeenCalled();
   });
 
 });
