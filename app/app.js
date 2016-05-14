2621c596c147e109f823f6a9f10af6b67c9e9006
nishant <nishant.singh87@gmail.com>
2016-05-14 20:09:38 +0530

    adding scroll interactoin.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,35 @@
 (function(){
   "use strict"
-  window.app = angular.module("amoeba", []);
+  var init = function(){
+    var lastScrollTop = 0,
+        readingOffset = 100,
+        topBarHeight = 85,
+        introOutAt = $(window).height() - topBarHeight - 60,
+        app = $("#app"),
+        reading = function(){
+          app.addClass("reading");
+        },
+        navigating = function(){
+          app.removeClass("reading");
+        };
+
+
+    $(window).on("scroll", function(){
+      var scollable    = $(this),
+          scrollTop     = scollable.scrollTop(),
+          nearTop       = scrollTop < readingOffset,
+          doneIntro     = introOutAt < scrollTop,
+          scrollingDown = scrollTop - lastScrollTop > 0;
+
+      doneIntro ? app.removeClass("intro") : app.addClass("intro");
+
+      (!nearTop && scrollingDown) ? reading() : navigating();
+      lastScrollTop = scrollTop;
+    });
+  };
+
+  $(document).ready(function(){
+    init();
+  });
+
 }).call(this);
