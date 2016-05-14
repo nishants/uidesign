f113212578c9c3f92085e114cb6fa1547a8dce5f
nishant <nishant.singh87@gmail.com>
2016-05-14 19:55:19 +0530

    checking compiled files.

--- serve/js/app.js
+++ serve/js/app.js
@@ -1,6 +1,32 @@
 (function(){
   "use strict"
-  window.app = angular.module("amoeba", []);
+  var init = function(){
+    var lastScrollTop = 0,
+        readingOffset = 50,
+        app = $("#app"),
+        reading = function(){
+          app.addClass("reading");
+        },
+        navigating = function(){
+          app.removeClass("reading");
+        };
+
+
+    $("body").on("scroll", function(){
+      var scollable = $(this),
+          scrollTop = scollable.scrollTop(),
+          nearTop = scrollTop < readingOffset,
+          scrollingDown = scrollTop - lastScrollTop > 0;
+
+      (!nearTop && scrollingDown) ? reading() : navigating();
+      lastScrollTop = scrollTop;
+    });
+  };
+
+  $(document).ready(function(){
+    init();
+  });
+
 }).call(this);
 (function(){
   "use strict"
