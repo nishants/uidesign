b580dbd2955d313893a21b4108ddc51f9079322f
nishant <nishant.singh87@gmail.com>
2016-05-15 12:12:11 +0530

    slate affect target on card (not gridbox)

--- app/deck/slate.js
+++ app/deck/slate.js
@@ -1,35 +1,35 @@
-//(function(){
-//  "use strict"
-//  app.directive("slate", [function () {
-//    var select = function (index) {
-//          var cardContainer = $($(".deck > li")[index]),
-//              card          = cardContainer.find(" > .card"),
-//              offsetPadding = 10,
-//              offsetY = $(".deck").offset().top - cardContainer.offset().top,
-//              offsetX = $(".deck").offset().left - cardContainer.offset().left;
-//
-//          card.css("transform", "translateY(" + (offsetY + offsetPadding) + "px)" + "translateX(" + (offsetX + offsetPadding) + "px)");
-//          $("body").css("overflow", "hidden")
-//        },
-//        unSelect = function (index) {
-//          var card = $($(".deck > li")[index]);
-//          card.css("transform", "");
-//          $("body").css("overflow", "auto")
-//        };
-//    return {
-//      restrict: "C",
-//      scope: true,
-//      transclude: false,
-//      link: function (scope, element, attrs) {
-//        scope.$watch("deck.current", function (now, previous) {
-//          now != -1 && select(now);
-//          previous != -1 && unSelect(previous);
-//          setTimeout(function(){
-//            $("body").stop().animate({scrollTop:$(element).offset().top}, '500', 'swing');
-//          },100);
-//        });
-//      }
-//    };
-//  }]);
-//
-//}).call(this);
+(function(){
+  "use strict"
+  app.directive("slate", [function () {
+    var select = function (index) {
+          var cardContainer = $($(".deck > li")[index]),
+              card          = cardContainer.find(".card").first(),
+              offsetPadding = 10,
+              offsetY = $(".deck").offset().top - cardContainer.offset().top,
+              offsetX = $(".deck").offset().left - cardContainer.offset().left;
+
+          card.css("transform", "translateY(" + (offsetY + offsetPadding) + "px)" + "translateX(" + (offsetX + offsetPadding) + "px)");
+          $("body").css("overflow", "hidden")
+        },
+        unSelect = function (index) {
+          var card = $($(".deck > li")[index]);
+          card.css("transform", "");
+          $("body").css("overflow", "auto")
+        };
+    return {
+      restrict: "C",
+      scope: true,
+      transclude: false,
+      link: function (scope, element, attrs) {
+        scope.$watch("deck.current", function (now, previous) {
+          now != -1 && select(now);
+          previous != -1 && unSelect(previous);
+          setTimeout(function(){
+            $("body").stop().animate({scrollTop:$(element).offset().top}, '500', 'swing');
+          },100);
+        });
+      }
+    };
+  }]);
+
+}).call(this);
