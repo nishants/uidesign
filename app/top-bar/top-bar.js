f8e5ac9a62ab2a0ac4e0d48d04590a7cea8881ce
nishant <nishant.singh87@gmail.com>
2016-05-15 10:48:05 +0530

    extracting top-bar.js.

---
+++ app/top-bar/top-bar.js
@@ -0,0 +1,40 @@
+(function(){
+  "use strict"
+  var init = function(){
+    var lastScrollTop   = 0,
+        readingOffset   = 100,
+        topBarHeight    = 85,
+        introOutAt      = $(window).height() - topBarHeight - 60,
+        app             = $("#app"),
+        reading = function(){
+          app.addClass("reading");
+        },
+        navigating = function(){
+          app.removeClass("reading");
+        };
+
+
+    var handleScroll = function (scollable) {
+      var scrollTop = scollable.scrollTop(),
+          nearTop = scrollTop < readingOffset,
+          doneIntro = introOutAt < scrollTop,
+          scrollingDown = scrollTop - lastScrollTop > 0;
+
+      doneIntro ? app.removeClass("intro") : app.addClass("intro");
+
+      (!nearTop && scrollingDown) ? reading() : navigating();
+      lastScrollTop = scrollTop;
+    };
+
+    $("#top-fixed-bar").on("mouseenter",navigating);
+
+    $(window).on("scroll", function(){
+      handleScroll($(this));
+    });
+  };
+
+  $(document).ready(function(){
+    init();
+  });
+
+}).call(this);
