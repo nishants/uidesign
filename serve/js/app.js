15bf6785d358c00fd1a37564667c1eec76c5efe9
nishant <nishant.singh87@gmail.com>
2016-05-15 10:52:34 +0530

    adding slate.js : compiled

--- serve/js/app.js
+++ serve/js/app.js
@@ -1,5 +1,14 @@
 (function(){
   "use strict"
+  var app = angular.module("amoeba", []);
+  window.app = app;
+}).call(this);
+(function(){
+  "use strict"
+console.log("routes")
+}).call(this);
+(function(){
+  "use strict"
   var init = function(){
     var lastScrollTop   = 0,
         readingOffset   = 100,
@@ -40,5 +49,35 @@
 }).call(this);
 (function(){
   "use strict"
-console.log("routes")
+  app.directive("slate", [function () {
+    var select = function (index) {
+          var card = $($(".deck > li")[index]),
+              offsetPadding = 10,
+              offsetY = $(".deck").offset().top - card.offset().top,
+              offsetX = $(".deck").offset().left - card.offset().left;
+
+          card.css("transform", "translateY(" + (offsetY + offsetPadding) + "px)" + "translateX(" + (offsetX + offsetPadding) + "px)");
+          $("body").css("overflow", "hidden")
+        },
+        unSelect = function (index) {
+          var card = $($(".deck > li")[index]);
+          card.css("transform", "");
+          $("body").css("overflow", "auto")
+        };
+    return {
+      restrict: "C",
+      scope: true,
+      transclude: false,
+      link: function (scope, element, attrs) {
+        scope.$watch("deck.current", function (now, previous) {
+          now != -1 && select(now);
+          previous != -1 && unSelect(previous);
+          setTimeout(function(){
+            $("body").stop().animate({scrollTop:$(element).offset().top}, '500', 'swing');
+          },100);
+        });
+      }
+    };
+  }]);
+
 }).call(this);
