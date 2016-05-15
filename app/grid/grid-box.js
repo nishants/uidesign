4d300248accb04373255b5404a40294b1ad00c9a
nishant <nishant.singh87@gmail.com>
2016-05-15 19:03:25 +0530

    smoothe scroll on card select.

--- app/grid/grid-box.js
+++ app/grid/grid-box.js
@@ -25,11 +25,19 @@
   };
 
   GridBox.prototype.select = function(){
-    $("body").css("overflow", "hidden")
-    var y = (this.yPosition - this.offset.y) - (this.$gridbox.offset().top - $(window).scrollTop()),
-        x = 0;
-    this.__applyPosition(x,y);
-    this.$gridbox.attr("data-grid-selected","true");
+    var self      = this,
+        $window   = $(window),
+        scroll    = $window.scrollTop() < self.$grid.offset().top,
+        selectBox = function(){
+          $("body").css("overflow", "hidden")
+          var y = (self.yPosition - self.offset.y) - (self.$gridbox.offset().top - $window.scrollTop()),
+              x = 0;
+          self.__applyPosition(x,y);
+          self.$gridbox.attr("data-grid-selected","true");
+        };
+
+        scroll && $('html, body').animate({scrollTop : this.$gridbox.offset().top}, 250, selectBox);
+        scroll || selectBox();
   };
 
   GridBox.prototype.setPosition = function(x,y){
