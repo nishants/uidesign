b0d996bd9e2d5502df37300456f8ac7230359545
nishant <nishant.singh87@gmail.com>
2016-05-24 12:00:52 +0530

    make logo in accessible.

--- serve/js/app.js
+++ serve/js/app.js
@@ -20,48 +20,6 @@ console.log("routes")
   }]);
 
 }).call(this);
-(function(){
-  "use strict"
-  var app = angular.module("editor", []);
-  app.directive("code", function($log){
-    var
-        defaultOptions = {
-          //remove default red dots for unknown character(linefeed, etc)
-          specialCharPlaceholder: function(){return document.createElement("span")},
-          readOnly: true,
-
-          // show line numbers in editor
-          lineNumbers: true,
-
-          // renders whole document at once (else only part in view is rendered, and text searches wont work)
-          //viewportMargin: Infinity,
-
-          // syntax highlighter mode
-          mode: "htmlmixed"
-        },
-
-
-        showError = function (err) {
-          $log.error(err);
-        },
-
-        initializeCodeMirror = function (textArea, editorOptions) {
-          CodeMirror.fromTextArea(textArea, editorOptions);
-        };
-
-    return {
-      scope: false,
-      transclude: false,
-      restrict: 'C',
-      link : function(scope, element, attrs){
-        var editorOptions = angular.extend(defaultOptions, {});
-        editorOptions.mode = attrs.mode || editorOptions.mode;
-
-        initializeCodeMirror(element[0], editorOptions);
-      }
-    };
-  })
-})();
 (function () {
   "use strict"
 
@@ -318,3 +276,45 @@ console.log("routes")
   });
 
 }).call(this);
+(function(){
+  "use strict"
+  var app = angular.module("editor", []);
+  app.directive("code", function($log){
+    var
+        defaultOptions = {
+          //remove default red dots for unknown character(linefeed, etc)
+          specialCharPlaceholder: function(){return document.createElement("span")},
+          readOnly: true,
+
+          // show line numbers in editor
+          lineNumbers: true,
+
+          // renders whole document at once (else only part in view is rendered, and text searches wont work)
+          //viewportMargin: Infinity,
+
+          // syntax highlighter mode
+          mode: "htmlmixed"
+        },
+
+
+        showError = function (err) {
+          $log.error(err);
+        },
+
+        initializeCodeMirror = function (textArea, editorOptions) {
+          CodeMirror.fromTextArea(textArea, editorOptions);
+        };
+
+    return {
+      scope: false,
+      transclude: false,
+      restrict: 'C',
+      link : function(scope, element, attrs){
+        var editorOptions = angular.extend(defaultOptions, {});
+        editorOptions.mode = attrs.mode || editorOptions.mode;
+
+        initializeCodeMirror(element[0], editorOptions);
+      }
+    };
+  })
+})();
