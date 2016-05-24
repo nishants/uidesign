5fed95efd80041bc25ada841ea93c1739467f47c
nishant <nishant.singh87@gmail.com>
2016-05-24 17:27:45 +0530

    watch selected card index.

--- app/grid/grid-controller.js
+++ app/grid/grid-controller.js
@@ -5,7 +5,11 @@
     $scope.deck = {
       current: -1,
       cards : snippetSerive.all()
-    }
+    };
 
+    $scope.$watch("deck.current", function(now, last){
+      last != -1 && console.log("closing :  " + $scope.deck.cards[last].title);
+      now  != -1 && console.log("closing :  " + $scope.deck.cards[now].title);
+    })
   }]);
 }).call(this);
--- serve/js/app.js
+++ serve/js/app.js
@@ -216,8 +216,12 @@ console.log("routes")
     $scope.deck = {
       current: -1,
       cards : snippetSerive.all()
-    }
+    };
 
+    $scope.$watch("deck.current", function(now, last){
+      last != -1 && console.log("closing :  " + $scope.deck.cards[last].title);
+      now  != -1 && console.log("closing :  " + $scope.deck.cards[now].title);
+    })
   }]);
 }).call(this);
 (function(){
