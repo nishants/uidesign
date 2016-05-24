4e09adcc0e5c3a14aab6cf8a940c108deff42a17
nishant <nishant.singh87@gmail.com>
2016-05-24 09:58:04 +0530

    adding header.

--- serve/js/app.js
+++ serve/js/app.js
@@ -7,6 +7,19 @@
   "use strict"
 console.log("routes")
 }).call(this);
+(function(){
+  "use strict"
+  app.directive("slate", [function () {
+    return {
+      restrict: "C",
+      scope: true,
+      transclude: false,
+      link: function (scope, element, attrs) {
+      }
+    };
+  }]);
+
+}).call(this);
 (function () {
   "use strict"
 
@@ -220,19 +233,6 @@ console.log("routes")
   };
 
   window.States = States;
-}).call(this);
-(function(){
-  "use strict"
-  app.directive("slate", [function () {
-    return {
-      restrict: "C",
-      scope: true,
-      transclude: false,
-      link: function (scope, element, attrs) {
-      }
-    };
-  }]);
-
 }).call(this);
 (function(){
   "use strict"
