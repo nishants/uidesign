c6463af3c5355952144bdafee0138d4cf981e857
nishant <nishant.singh87@gmail.com>
2016-08-15 12:17:47 +0530

    show progress bar by no. of success scenarios.

--- practice-labs/ruby-for-qa/js/app.js
+++ practice-labs/ruby-for-qa/js/app.js
@@ -51,6 +51,9 @@
       },
       console  : {
         show: false,
+        output : {
+          scenarios: []
+        }
       },
       ace: ace.edit("editor"),
       contentChanged: function(e){
@@ -79,5 +82,22 @@
       }, editor.autoRun.interval);
     });
     $scope.editor = editor;
+  }]);
+
+  app.directive("taskStatusBar", [function(){
+    return {
+      restrict: "A",
+      scope   : true,
+      link    : function(scope, element, attrs){
+        scope.$watch("editor.console.output.scenarios", function(now, then){
+          var scenarios = scope.editor.console.output.scenarios;
+          var done = !scenarios ? 0 : scenarios.filter(function (scenario) {
+            return scenario.success;
+          }).length;
+          var todo = scope.editor.console.output.scenarios.length;
+          element.css("transform","scaleX(:progress)".replace(":progress", done/todo))
+        });
+      }
+    };
   }]);
 }).call(this);
