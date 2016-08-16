2f265eee2ef23757d365f51ae74e61c820f8e139
nishant <nishant.singh87@gmail.com>
2016-08-16 15:38:28 +0530

    do not highlight white space is scenario is success

--- practice-labs/ruby-for-qa/js/editor/white-space-diff.js
+++ practice-labs/ruby-for-qa/js/editor/white-space-diff.js
@@ -3,8 +3,11 @@
   app.directive("whiteSpaceDiff", [function(){
 
     var formatWhiteSpace = function(value){
-      return "<span>" + JSON.stringify(value).split(" ").join('</span><span class="white-space"></span><span>') + "</span>"
-    };
+          return "<span>" + JSON.stringify(value).split(" ").join('</span><span class="white-space"></span><span>') + "</span>"
+        },
+        noWhiteSpace = function(value){
+          return JSON.stringify(value).split(" ").join("");
+        };
 
     return {
       restrict: "A",
@@ -13,14 +16,14 @@
         var whiteSpaceDiff = {};
         scope.$watch(attrs.whiteSpaceDiff, function(scenario){
           if(!scenario) return;
-          var isDifferent    = !scenario.success,
-              expected       = isDifferent ? JSON.stringify(scenario.expected).split(" ").join("") : scenario.expected,
-              actual         = isDifferent ? JSON.stringify(scenario.actual).split(" ").join("")   : scenario.actual,
-              whiteDiffFound = (expected === actual);
+          var diffFound      = !scenario.success,
+              expected       = scenario.expected,
+              actual         = scenario.actual,
+              whiteDiffFound = diffFound && (noWhiteSpace(expected) === noWhiteSpace(actual));
 
           if(whiteDiffFound){
-            expected = formatWhiteSpace(scenario.expected);
-            actual   = formatWhiteSpace(scenario.actual);
+            expected = formatWhiteSpace(expected);
+            actual   = formatWhiteSpace(actual);
           }
 
           whiteSpaceDiff.expected = expected;
