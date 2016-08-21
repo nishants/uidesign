872e2112b829fcc311246f9e30ace51ddf71b152
nishant <nishant.singh87@gmail.com>
2016-08-21 16:45:21 +0530

    svg handler.

--- clients/mphasis/testing/js/app.js
+++ clients/mphasis/testing/js/app.js
@@ -1,5 +1,5 @@
 (function(){
   "use strict"
   var app = angular.module("presenter", []);
-
+  window.app = app;
 }).call(this);
---
+++ clients/mphasis/testing/js/svg.js
@@ -0,0 +1,14 @@
+(function(){
+  "use strict"
+  app.directive("svg", [function(){
+    return {
+      restrict  : "E",
+      scope     : false,
+      transclude: false,
+      link      : function(scope, element, attrs){
+        element.attr("width", "100%")
+      }
+    };
+  }]);
+
+}).call(this);
