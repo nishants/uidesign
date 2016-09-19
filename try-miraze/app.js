2ccc5c6be8522fc6cc38d25c81ed1a2a3cf548e6
nishant <nishant.singh87@gmail.com>
2016-09-19 18:02:23 +0530

    json editor for request body.

--- try-miraze/app.js
+++ try-miraze/app.js
@@ -1,6 +1,30 @@
 (function () {
   "use strict"
 
-  window.app = angular.module("miraze-play", []);
+  window.app = angular.module("miraze-play", ["ng.jsoneditor"]);
 
+  app.controller("ui-controller", function($scope){
+    var requestBody = {
+      options: {
+        mode: 'code',
+        change: function(){
+          console.log("code changed");
+        },
+        onLoad: function (instance) {
+          requestBody.editor = instance;
+          instance.expandAll();
+        }
+      },
+      content: {
+        "data": {
+          "message": "Hello World"
+        },
+
+      },
+      onLoad: function () {
+        console.log("loaded");
+      }
+    };
+    $scope.requestBody = requestBody
+  })
 }).call(this);
