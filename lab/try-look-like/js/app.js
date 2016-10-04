25a3833548d87fba04aa6d3e7f823b0c3fae0297
nishant <nishant.singh87@gmail.com>
2016-10-04 21:18:56 +0530

    run and show result.

--- lab/try-look-like/js/app.js
+++ lab/try-look-like/js/app.js
@@ -1,9 +1,12 @@
 (function () {
   "use strict"
   var app = angular.module("try-look-like", []);
-  app.service("aceEditor", [function () {
+  app.service("aceEditor", ["$http", function ($http) {
 
     return {
+      run : function(script){
+        return $http.put("https://amoeba-social-look-like-server.herokuapp.com/assertion/evaluate", script);
+      },
       create: function (elment) {
         var aceEditor = ace.edit(elment);
         aceEditor.getSession().setMode("ace/mode/ruby");
@@ -18,7 +21,14 @@
 
   app.controller("editorController", ["$scope", "aceEditor", function ($scope, aceEditor) {
     var editor = {
-          ace: aceEditor.create("editor-container")
+          ace: aceEditor.create("editor-container"),
+          run: function(){
+            aceEditor.run(editor.ace.getValue()).then(function(response){
+              console.errors = response.data
+            }, function(){
+              alert("Unknown error!")
+            })
+          }
         },
         console = {
           errors: ["some error"]
