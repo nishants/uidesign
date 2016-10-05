9de0c9624e42ce85658286f4e2a67de9d00efc18
nishant <nishant.singh87@gmail.com>
2016-10-05 11:40:08 +0530

    load samples from config.

---
+++ lab/try-look-like/config/samples.rb
@@ -0,0 +1,41 @@
+#Email
+expect("one@two.xyz").to look_like("email")
+expect("one@two.xyz").to look_like("a@b.com")
+
+#Amount and Currency
+expect("$53,23,1").to look_like("$amount")
+expect("₹23,1.00").to look_like("₹amount")
+
+expect("$53,23,1").to look_like("$12.21")
+expect("₹23,1.00").to look_like("₹100.12")
+
+#Enums
+expect("one").to look_like("one/two/three")
+expect("four").not_to look_like("one/two/three")
+Regex
+expect("i have  test").to look_like("/test/")
+expect("i have  tess").not_to look_like("/test/")
+
+#URL
+expect("google.com").to look_like("http://google.com")
+expect("http://google.com").to look_like("http://google.com")
+expect("google-com").not_to look_like("http://google.com")
+
+#Wildcard
+expect("").to look_like("email*")
+expect("not.an.email").not_to look_like("email*")
+expect("one@two.xyz").to look_like("email*")
+
+expect("one@two.xyz").to look_like("a@b.com*")
+
+expect("").to look_like("*")
+expect("any-thing").to look_like("*")
+
+#Numbers
+expect("5000").to look_like("number")
+expect("5,000").to look_like("number")
+expect("5,43.11").to look_like("number")
+
+expect("6993").to look_like("5000")
+expect("5000").to look_like("5,000")
+expect("$5000").not_to look_like("5000")
--- lab/try-look-like/js/app.js
+++ lab/try-look-like/js/app.js
@@ -2,7 +2,12 @@
   "use strict"
   var app = angular.module("try-look-like", []);
   app.service("aceEditor", ["$http", function ($http) {
-
+    var createSample = function(config){
+      return {
+        name: config[0],
+        snippet : config.slice(1)
+      }
+    };
     return {
       run : function(script){
         return $http.put("https://amoeba-social-look-like-server.herokuapp.com/assertion/evaluate", script);
@@ -15,6 +20,17 @@
         aceEditor.getSession().setUseSoftTabs(true);
 
         return aceEditor;
+      },
+      getAllSamples : function(){
+        return $http.get("config/samples.rb").then(function(response){
+          return response.data.split("#").map(function(sample){
+            return sample.split("\n");
+          }).filter(function(sample){
+            return sample.length > 1;
+          }).map(function(sample){
+            return createSample(sample)
+          });
+        });
       }
     };
   }]);
@@ -30,16 +46,26 @@
             }, function(){
               alert("Unknown error!");
             })
-          }
+          },
+          samples: []
         },
         console = {
           running: false,
           errors: []
         };
+
     editor.ace.setValue('expect("one.two.xyz").to look_like("email") \nexpect("one.two.com").to look_like("email") \nexpect("one@two.xyz").to look_like("email")')
     $scope.editor = editor;
     $scope.console = console;
     editor.run();
+
+    $scope.$on('$routeChangeStart', function(next, current) {
+      console.log("hello");
+    });
+
+    aceEditor.getAllSamples().then(function(samples){
+      editor.samples = samples;
+    });
   }])
 
 }).call(this);
