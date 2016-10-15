034ffa89551b4f16688b20b55cdd7347fbc08e6e
nishant <nishant.singh87@gmail.com>
2016-10-15 23:44:44 +0530

    page styling.

--- practice-labs/ruby-for-qa/exercise-two/js/task/task-controller.js
+++ practice-labs/ruby-for-qa/exercise-two/js/task/task-controller.js
@@ -1,10 +1,17 @@
 (function(){
   "use strict"
-  window.app.controller("taskController", ["$scope", "uiService", function($scope, uiService){
+  window.app.controller("taskController", ["$scope", "uiService", "$http", function($scope, uiService, $http){
     var task = {
       readmeTemplate : "task/readme.html",
+      htmlPage : "task/page.html",
+      data    : "task/data.json",
+      page     : {},
       scenarios: {}
     };
+
+    $http.get(task.data).then(function(response){
+      $scope.page = response.data;
+    });
 
     $scope.task = task;
   }]);
