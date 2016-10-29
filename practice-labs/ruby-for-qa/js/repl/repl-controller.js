9bb2eaa829e0bde36bb3bba799249cfed1527c61
nishant <nishant.singh87@gmail.com>
2016-10-29 16:51:05 +0530

    show previous commands and results.

--- practice-labs/ruby-for-qa/js/repl/repl-controller.js
+++ practice-labs/ruby-for-qa/js/repl/repl-controller.js
@@ -5,14 +5,26 @@
     var repl = {
       output    : "",
       input     : "",
-      history   : [],
+      clear     : function(){
+        repl.history = [];
+      },
+      history   : [
+        {
+          input  : "puts 'hello'",
+          output : "hello"
+        }
+      ],
       enter     : function(event){
         event.code == "Enter" && repl.run() ;
       },
       run       : function () {
         return taskService.evaluate(repl.input).then(function (result) {
+          repl.history.push(        {
+                input  : repl.input,
+                output : JSON.stringify(result)
+              }
+          );
           repl.input = "";
-          repl.history.push(JSON.stringify(result));
         });
       }
     };
