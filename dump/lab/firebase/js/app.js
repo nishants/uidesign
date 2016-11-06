f27b02aad6d343fa928ad889f356a7859c7dd177
nishant <nishant.singh87@gmail.com>
2016-11-06 20:10:57 +0530

    extract remote-service.js

--- dump/lab/firebase/js/app.js
+++ dump/lab/firebase/js/app.js
@@ -10,53 +10,6 @@ app.value("credentials", {
   messagingSenderId: "188124641306"
 })
 
-app.service("RemoteService", ["credentials", function (credentials) {
-  var app = firebase.initializeApp(credentials),
-      onAuth = function () {
-      },
-      setAuthCallback = function (callback) {
-        onAuth = callback;
-      };
-
-  app.auth().onAuthStateChanged(function (user) {
-    onAuth(user)
-  });
-
-  var
-      signIn = function (userEmail, userPassword) {
-        return app.auth().signInWithEmailAndPassword(userEmail, userPassword)
-            .catch(function (error) {
-              console.error(error.message);
-            });
-      },
-      signOut = function (userEmail, userPassword) {
-        return app.auth().signOut()
-            .catch(function (error) {
-              console.error(error.message);
-            });
-      },
-      createAccount = function (userEmail, userPassword) {
-        return app.auth().createUserWithEmailAndPassword(userEmail, userPassword);
-      },
-      writeUserData = function (userId, name, picture) {
-        return app.database().ref('users/' + userId + "/profile").set({
-          name: name,
-          picture: picture
-        });
-      },
-      currentUser = function(){
-        return app.auth().currentUser;
-      };
-
-  return {
-    signIn: signIn,
-    signOut: signOut,
-    createAccount: createAccount,
-    writeUserData: writeUserData,
-    onAuth: setAuthCallback,
-    currentUser: currentUser,
-  };
-}]);
 
 app.service("UserService", ["RemoteService", function (remoteService) {
   var service = {
---
+++ dump/lab/firebase/js/services/remote-service.js
@@ -0,0 +1,47 @@
+app.service("RemoteService", ["credentials", function (credentials) {
+  var app = firebase.initializeApp(credentials),
+      onAuth = function () {
+      },
+      setAuthCallback = function (callback) {
+        onAuth = callback;
+      };
+
+  app.auth().onAuthStateChanged(function (user) {
+    onAuth(user)
+  });
+
+  var
+      signIn = function (userEmail, userPassword) {
+        return app.auth().signInWithEmailAndPassword(userEmail, userPassword)
+            .catch(function (error) {
+              console.error(error.message);
+            });
+      },
+      signOut = function (userEmail, userPassword) {
+        return app.auth().signOut()
+            .catch(function (error) {
+              console.error(error.message);
+            });
+      },
+      createAccount = function (userEmail, userPassword) {
+        return app.auth().createUserWithEmailAndPassword(userEmail, userPassword);
+      },
+      writeUserData = function (userId, name, picture) {
+        return app.database().ref('users/' + userId + "/profile").set({
+          name: name,
+          picture: picture
+        });
+      },
+      currentUser = function(){
+        return app.auth().currentUser;
+      };
+
+  return {
+    signIn: signIn,
+    signOut: signOut,
+    createAccount: createAccount,
+    writeUserData: writeUserData,
+    onAuth: setAuthCallback,
+    currentUser: currentUser,
+  };
+}]);
