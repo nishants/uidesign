13afbaa7c71d5c44516a0d5c61fec9d013013476
nishant <nishant.singh87@gmail.com>
2017-02-27 15:40:11 +0530

    save item.

--- dump/lab/filepicker/index.js
+++ dump/lab/filepicker/index.js
@@ -8,7 +8,50 @@ app.constant("FILEPICKER", {
 	}
 });
 
+app.value("Remote", {
+	url: "https://fireapp-web-service.herokuapp.com/items"
+});
 
+app.controller("FormController", ["$scope", "$http", "Remote", function($scope, $http, Remote){
+	var form = {
+		name  : "Saree",
+		title  : "Premium Blue Benarasi Saree",
+		description  : "Made with blue silk and golden threads. Be the royalty to the party.",
+		saving: false,
+		images: [
+			{name: "one", url: "https://cdn.filestackcontent.com/FV8pOUsxRpiA56uC7306"},
+			{name: "two", url: "https://cdn.filestackcontent.com/azOXY2iASm6clQ3YVTDg"},
+			{name: "three", url: "https://cdn.filestackcontent.com/FV8pOUsxRpiA56uC7306"},
+			{name: "four", url: "https://cdn.filestackcontent.com/azOXY2iASm6clQ3YVTDg"}
+		],
+		serialize: function () {
+			return {
+				data: {
+					name 				: form.name,
+					title 			: form.title,
+					description : form.description,
+					images  		: form.images
+				}
+			};
+		},
+		save: function () {
+			var create = function(){
+				$http.post(Remote.url, form.serialize()).then(function (response) {
+					form.id = response.id;
+					form.saving = false;
+				});
+			}, save = function(){
+				$http.put(Remote.url, form.serialize()).then(function (response) {
+					form.saving = false;
+				})
+			};
+			form.saving = true;
+			form.id ? save() : create();
+		}
+	};
+
+	$scope.form = form;
+}]);
 app.directive("imagepicker", ["FILEPICKER", "$timeout", function(FILEPICKER, $timeout){
 	return{
 		restrict: "A",
