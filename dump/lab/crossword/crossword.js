4684d41b75ca462f92d02f0725381c4c93da42c4
nishant <nishant.singh87@gmail.com>
2017-04-19 23:18:05 +0530

    adding crossword.

---
+++ dump/lab/crossword/crossword.js
@@ -0,0 +1,22 @@
+var app = angular.module("crossword", []);
+app.controller("CrosswordController", ["$scope", function($scope){
+	$scope.crossword={
+		cellWidth 	: 30,
+		cellHeight  : 30,
+		cellsPerRow : 10,
+		crosswordLayout:{
+			cells : [
+				true,  true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+				false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+				false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+				false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+				false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+				false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+				false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+				false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+				false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+			]
+		},
+		inputLayout: {}
+	}
+}]);
