8d3ad8ac4283ee1641df5df4cb865ac30e20081c
nishant <nishant.singh87@gmail.com>
2017-04-20 13:36:06 +0530

    capture key events.

--- dump/lab/crossword/crossword.js
+++ dump/lab/crossword/crossword.js
@@ -1,7 +1,16 @@
 var app = angular.module("crossword", []);
 app.controller("CrosswordController", ["$scope", function ($scope) {
 
-	var createCell = function(param){
+	var isNextChar = function(keyCode){
+				var isUppercase = keyCode > 64 && keyCode < 91,
+						isLowercase = keyCode > 96 && keyCode < 123;
+				return isUppercase || isLowercase;
+			},
+
+			isBackChar = function(keyCode){
+				return keyCode == 8;
+			},
+			createCell = function(param){
 				return {
 					label: param.label,
 					value: null,
@@ -23,7 +32,9 @@ app.controller("CrosswordController", ["$scope", function ($scope) {
 				select: function(cellIndex){
 					crossword.focus = cellIndex;
 				},
-				input: function(event){
+				input: function(index, event){
+					isNextChar(event.keyCode) ? console.log("going to next"): "";
+					isBackChar(event.keyCode) ? console.log("going to previous"): "";
 					console.log(event.keyCode);
 				}
 			};
