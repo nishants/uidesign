11d70cc23b955ab7435a5586a02f1b8ec2d3c15b
nishant <nishant.singh87@gmail.com>
2017-04-20 11:55:40 +0530

    adding vertical input.

--- dump/lab/crossword/crossword.js
+++ dump/lab/crossword/crossword.js
@@ -7,17 +7,26 @@ app.controller("CrosswordController", ["$scope", function($scope){
 		update: function(input){
 			var fromIndex = input.index,
 					cells = crossword.crosswordLayout.cells;
-			for(var i = 0; i< input.length; i++){
-				cells[i + fromIndex].value = input.value[i] || null;
+			if(!input.vertical){
+				for(var i = 0; i< input.length; i++){
+					cells[i + fromIndex].value = input.value[i] || null;
+				}
 			}
+
+			if(input.vertical){
+				for(var i = 0; i< input.length; i++){
+					cells[i*crossword.cellsPerRow + fromIndex].value = input.value[i] || null;
+				}
+			}
+
 		},
 		crosswordLayout:{
 			cells : [
 				{value: "", solid: false},  {value: "", solid: false}, {value: "", solid: false}, {value: "", solid: false}, {value: "", solid: false}, {value: "", solid: false}, {value: "", solid: false}, {value: "", solid: false}, {value: "", solid: false}, {value: "", solid: false},
-				{value: "", solid: true },  {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true },
-				{value: "", solid: true },  {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true },
-				{value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },
-				{value: "", solid: true },  {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true },
+				{value: "", solid: false },  {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true },
+				{value: "", solid: false },  {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true },
+				{value: "", solid: false },  {value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },  {value: "", solid: true },
+				{value: "", solid: false },  {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true },
 				{value: "", solid: true },  {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true },
 				{value: "", solid: true },  {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true },
 				{value: "", solid: true },  {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true }, {value: "", solid: true },
@@ -31,10 +40,18 @@ app.controller("CrosswordController", ["$scope", function($scope){
 					index: 0,
 					length: 10,
 					vertical: false,
+				},
+				{
+					value: "bcde",
+					index: 10,
+					length: 4,
+					vertical: true,
 				}
+
 			]
 		}
 	};
 
 	crossword.update(crossword.inputLayout.inputs[0]);
+	crossword.update(crossword.inputLayout.inputs[1]);
 	$scope.crossword= crossword}]);
