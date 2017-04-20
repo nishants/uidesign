f2cbcc232f14c8005a1c86304eb793df3b6eb77b
nishant <nishant.singh87@gmail.com>
2017-04-20 11:45:28 +0530

    crossword input word.

--- dump/lab/crossword/crossword.js
+++ dump/lab/crossword/crossword.js
@@ -1,9 +1,16 @@
 var app = angular.module("crossword", []);
 app.controller("CrosswordController", ["$scope", function($scope){
-	$scope.crossword={
+	var crossword = {
 		cellWidth 	: 30,
 		cellHeight  : 30,
 		cellsPerRow : 10,
+		update: function(input){
+			var fromIndex = input.index,
+					cells = crossword.crosswordLayout.cells;
+			for(var i = 0; i< input.length; i++){
+				cells[i + fromIndex].value = input.value[i] || null;
+			}
+		},
 		crosswordLayout:{
 			cells : [
 				{value: "", solid: false},  {value: "", solid: false}, {value: "", solid: false}, {value: "", solid: false}, {value: "", solid: false}, {value: "", solid: false}, {value: "", solid: false}, {value: "", solid: false}, {value: "", solid: false}, {value: "", solid: false},
@@ -27,5 +34,7 @@ app.controller("CrosswordController", ["$scope", function($scope){
 				}
 			]
 		}
-	}
-}]);
+	};
+
+	crossword.update(crossword.inputLayout.inputs[0]);
+	$scope.crossword= crossword}]);
