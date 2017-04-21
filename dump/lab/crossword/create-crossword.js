4fb96b8ff41ce74e6ec604c1bf1adc3c00c4f480
nishant <nishant.singh87@gmail.com>
2017-04-21 13:53:26 +0530

    extract crossword directive.

--- dump/lab/crossword/create-crossword.js
+++ dump/lab/crossword/create-crossword.js
@@ -1,186 +1,177 @@
 var app = angular.module("create-crossword", []);
-app.controller("MatrixController", ["$scope", function ($scope) {
-
-	var matrix = {
-		word: null,
-		relatives: [],
-		setRoot: function(word){
-			matrix.word = word;
-		},
-		addRelative: function(word){
-			var rootWordLetters = matrix.word.split(""),
-					crosses = [];
-
-			rootWordLetters.forEach(function(letter, index){
-				crosses.push({
-					indexInStem : index,
-					indexInWord : word.indexOf(letter),
-				});
-			});
-
-			matrix.relatives.push({
-				word: word,
-				crosses: crosses
-			});
-		},
-		crossRelations: function(){
-			var crosses = matrix.word.split("").map(function(){return null;}),
-					positions = [],
-					unrelated = [],
-					occupied = [];
-
-			matrix.relatives.forEach(function(relative, relativeIndex){
-				var
-						even = relativeIndex%2 == 0,
-						crosses = relative.crosses.filter(function (cross) {
-							return cross.indexInWord > -1 && occupied.indexOf(cross.indexInStem) == -1;
-						}).sort(),
-						index = even ? crosses[crosses.length -1] : crosses[0];
-
-				if(index){
-					occupied.push(index.indexInStem);
-					positions.push({
-						index : index,
-						word  : relative.word,
-						vertical: true
-					});
-				} else{
-					unrelated.push({word  : relative.word});
-				}
-			});
-
-			return  {
-				stem: {
-					index : 0,
-					word  : matrix.word,
-					vertical: false
+app.service("Matrix", [function () {
+	return {
+		create: function(){
+			var matrix = {
+				word: null,
+				relatives: [],
+				setRoot: function(word){
+					matrix.word = word;
 				},
-				relatives: positions,
-				unrelated: unrelated
-			};
-		},
-		calculateGrid: function(structure){
-			var columns		= 0,
-					rows 			= 0,
-					maxAboveStem = 0,
-					maxBelowStem = 0,
-					gridHeight   = 0,
-					gridWidth   = 0,
-					positions = [];
-			structure.relatives.forEach(function(relative){
-				var aboveStem = relative.index.indexInWord,
-						belowStem = relative.word.length - relative.index.indexInWord -1;
-				maxAboveStem = Math.max(maxAboveStem, aboveStem);
-				maxBelowStem = Math.max(maxBelowStem, belowStem);
-				positions.push({
-					word: relative.word,
-					col: relative.index.indexInStem,
-					row: relative.index.indexInWord,
-					vertical: true,
-				});
-			});
-			gridWidth = structure.stem.word.length;
-			gridHeight = maxBelowStem + maxAboveStem +1;
-			positions.push({
-				word: structure.stem.word,
-				col: 0,
-				row: 0,
-				vertical: false,
-			});
+				addRelative: function(word){
+					var rootWordLetters = matrix.word.split(""),
+							crosses = [];
 
-			positions = positions.map(function(position){
-				position.row = maxAboveStem - position.row;
-				return position;
-			});
+					rootWordLetters.forEach(function(letter, index){
+						crosses.push({
+							indexInStem : index,
+							indexInWord : word.indexOf(letter),
+						});
+					});
 
-			var appendToRight = function(unreleated){
-						gridWidth += 2;
-						return {
-							word: unreleated.word,
-							col : gridWidth -1,
-							row :0,
+					matrix.relatives.push({
+						word: word,
+						crosses: crosses
+					});
+				},
+				crossRelations: function(){
+					var crosses = matrix.word.split("").map(function(){return null;}),
+							positions = [],
+							unrelated = [],
+							occupied = [];
+
+					matrix.relatives.forEach(function(relative, relativeIndex){
+						var
+								even = relativeIndex%2 == 0,
+								crosses = relative.crosses.filter(function (cross) {
+									return cross.indexInWord > -1 && occupied.indexOf(cross.indexInStem) == -1;
+								}).sort(),
+								index = even ? crosses[crosses.length -1] : crosses[0];
+
+						if(index){
+							occupied.push(index.indexInStem);
+							positions.push({
+								index : index,
+								word  : relative.word,
+								vertical: true
+							});
+						} else{
+							unrelated.push({word  : relative.word});
+						}
+					});
+
+					return  {
+						stem: {
+							index : 0,
+							word  : matrix.word,
+							vertical: false
+						},
+						relatives: positions,
+						unrelated: unrelated
+					};
+				},
+				calculateGrid: function(structure){
+					var columns		= 0,
+							rows 			= 0,
+							maxAboveStem = 0,
+							maxBelowStem = 0,
+							gridHeight   = 0,
+							gridWidth   = 0,
+							positions = [];
+					structure.relatives.forEach(function(relative){
+						var aboveStem = relative.index.indexInWord,
+								belowStem = relative.word.length - relative.index.indexInWord -1;
+						maxAboveStem = Math.max(maxAboveStem, aboveStem);
+						maxBelowStem = Math.max(maxBelowStem, belowStem);
+						positions.push({
+							word: relative.word,
+							col: relative.index.indexInStem,
+							row: relative.index.indexInWord,
 							vertical: true,
-						};
-					},
-					appendToBottom = function(unreleated){
-						gridHeight += 2;
-						return {
-							word: unreleated.word,
-							col : 0,
-							row : gridHeight -1,
-							vertical: false,
-						};
+						});
+					});
+					gridWidth = structure.stem.word.length;
+					gridHeight = maxBelowStem + maxAboveStem +1;
+					positions.push({
+						word: structure.stem.word,
+						col: 0,
+						row: 0,
+						vertical: false,
+					});
+
+					positions = positions.map(function(position){
+						position.row = maxAboveStem - position.row;
+						return position;
+					});
+
+					var appendToRight = function(unreleated){
+								gridWidth += 2;
+								return {
+									word: unreleated.word,
+									col : gridWidth -1,
+									row :0,
+									vertical: true,
+								};
+							},
+							appendToBottom = function(unreleated){
+								gridHeight += 2;
+								return {
+									word: unreleated.word,
+									col : 0,
+									row : gridHeight -1,
+									vertical: false,
+								};
+							};
+
+					structure.unrelated.forEach(function(unrelated, unrelatedIndex){
+						var even 		 = unrelatedIndex%2 == 0,
+								position = even ? appendToRight(unrelated) : appendToBottom(unrelated);
+						positions.push(position);
+					});
+
+					return {
+						columns  : gridWidth,
+						rows : gridHeight,
+						words: positions
 					};
+				},
+				createCells: function(grid){
+					var cells = new Array(grid.columns * grid.rows).fill({solid: true});
 
-			structure.unrelated.forEach(function(unrelated, unrelatedIndex){
-				var even 		 = unrelatedIndex%2 == 0,
-						position = even ? appendToRight(unrelated) : appendToBottom(unrelated);
-				positions.push(position);
-			});
+					grid.words.forEach(function(word, index){
+						if(!word.vertical){
+							var wordBeginCell = grid.columns * word.row + word.col;
+							cells[wordBeginCell] = {
+								label:  index + 1,
+								solid: false,
+								expected: word.word[0]
+							};
 
-			return {
-				columns  : gridWidth,
-				rows : gridHeight,
-				words: positions
-			};
-		},
-		createCells: function(grid){
-			var cells = new Array(grid.columns * grid.rows).fill({solid: true});
-
-			grid.words.forEach(function(word, index){
-				if(!word.vertical){
-					var wordBeginCell = grid.columns * word.row + word.col;
-					cells[wordBeginCell] = {
-							label:  index + 1,
-							solid: false,
-							expected: word.word[0]
-					};
-
-					for(var i = 1; i < word.word.length; i++){
-						cells[wordBeginCell+i] = {
-							solid: false,
-							expected: word.word[i],
-							label: cells[wordBeginCell+i].label
+							for(var i = 1; i < word.word.length; i++){
+								cells[wordBeginCell+i] = {
+									solid: false,
+									expected: word.word[i],
+									label: cells[wordBeginCell+i].label
+								}
+							}
 						}
-					}
-				}
-				if(word.vertical){
-					var wordBeginCell = grid.columns * word.row + word.col;
-					cells[wordBeginCell] = {
-						label:  index + 1,
-						solid: false,
-						expected: word.word[0]
-					};
+						if(word.vertical){
+							var wordBeginCell = grid.columns * word.row + word.col;
+							cells[wordBeginCell] = {
+								label:  index + 1,
+								solid: false,
+								expected: word.word[0]
+							};
 
-					for(var i = 1; i < word.word.length; i++){
-						cells[wordBeginCell+i*grid.columns] = {
-							solid: false,
-							expected: word.word[i]
+							for(var i = 1; i < word.word.length; i++){
+								cells[wordBeginCell+i*grid.columns] = {
+									solid: false,
+									expected: word.word[i]
+								}
+							}
 						}
-					}
+
+					});
+
+					return {
+						cells: cells,
+						rows  : grid.rows,
+						columns: grid.columns,
+					};
 				}
-
-			});
-
-			return {
-				cells: cells,
-				rows  : grid.rows,
-				columns: grid.columns,
 			};
+
+			return matrix;
 		}
-	};
-
-	matrix.setRoot("demanding");
-	matrix.addRelative("monday");
-	matrix.addRelative("daring");
-	matrix.addRelative("india");
-	matrix.addRelative("tuberculosis");
-	matrix.addRelative("magician");
-	matrix.addRelative("fabrication");
-	matrix.addRelative("intensity");
-	matrix.addRelative("electrical");
-	matrix.addRelative("magnificent");
-	$scope.layout = matrix.createCells(matrix.calculateGrid(matrix.crossRelations()));
-
-	$scope.matrix = matrix;
+	}
 }]);
--- dump/lab/crossword/crossword.js
+++ dump/lab/crossword/crossword.js
@@ -1,86 +1,16 @@
-var app = angular.module("crossword", []);
-app.controller("CrosswordController", ["$scope", function ($scope) {
+var app = angular.module("crossword", ['create-crossword']);
 
-	var KEY_CODES = {
-		LEFT	: 37,
-		UP	  : 38,
-		RIGHT : 39,
-		DOWN	: 40,
-		BACKSPACE 	: 8,
-	};
-
-	var isNextChar = function(keyCode){
-				var isUppercase = keyCode > 64 && keyCode < 91,
-						isLowercase = keyCode > 96 && keyCode < 123,
-						isInput    = isUppercase || isLowercase,
-						isSkip     = [KEY_CODES.RIGHT, KEY_CODES.DOWN].indexOf(keyCode) != -1;
-				return isInput || isSkip;
-			},
-
-			isBackChar = function(keyCode){
-				return [KEY_CODES.BACKSPACE, KEY_CODES.UP, KEY_CODES.LEFT].indexOf(keyCode) != -1;
-			},
-			createCell = function(param){
-				return {
-					label: param.label,
-					value: null,
-					solid: param.solid
-				};
-			},
-			crossword = {
-				cellHeight: 50,
-				cellWidth: 50,
-				cellsPerRow: 5,
-				_goingDown: false,
-				cells: [
-					createCell({label: 1}), createCell({}), createCell({}), createCell({}), createCell({}),
-					createCell({}), createCell({solid: true}), createCell({solid: true}), createCell({solid: true}), createCell({solid: true}),
-					createCell({}), createCell({}), createCell({}), createCell({}), createCell({}),
-					createCell({}), createCell({}), createCell({}), createCell({}), createCell({}),
-				],
-				select: function(cellIndex){
-					crossword.focusOn(cellIndex);
-				},
-				_goToPreviousOf: function(index){
-					var
-							topIndex  = index - crossword.cellsPerRow,
-							leftIndex = index - 1,
-							hasTop   = topIndex > -1,
-							hasLeft  = leftIndex % crossword.cellsPerRow != (crossword.cellsPerRow -1),
-							previousIndex = hasLeft  ? leftIndex  : (hasTop ? topIndex : -1);
-
-
-					crossword.focusOn(previousIndex);
-				},
-				_goToNextOf: function(index){
-
-					var
-							bottomIndex  = index + crossword.cellsPerRow,
-							rightIndex   = index + 1,
-							hasBottom    = bottomIndex < crossword.cells.length && !crossword.cells[bottomIndex].solid,
-							hasRight     = rightIndex % crossword.cellsPerRow != 0 && !crossword.cells[rightIndex].solid,
-							goDown       = hasBottom && ( !hasRight || crossword._goingDown),
-							nextIndex    = goDown ? bottomIndex : hasRight ? rightIndex : -1 ;
-
-					crossword._goingDown = goDown;
-					crossword.focusOn(nextIndex);
-				},
-				input: function(index, event){
-					isNextChar(event.keyCode) ? crossword._goToNextOf(index): "";
-					isBackChar(event.keyCode) ? crossword._goToPreviousOf(index): "";
-				},
-				focusOn : function(index){
-					var cell   = angular.element(document.querySelector(".crossword li.cell:nth-child(:index)".replace(":index", index+1))),
-							cells  = angular.element(document.querySelectorAll(".crossword li.cell")),
-							input  = cell.find("input")[0];
-					cells.removeClass("focus");
-
-					if(index > -1){
-						input.select();
-						cell.addClass("focus");
-						input.focus();
-					}
-				}
-			};
-	$scope.crossword = crossword
+app.controller("CrosswordController", ["$scope", "Matrix", function ($scope, Matrix) {
+	var matrix = Matrix.create();
+	matrix.setRoot("demanding");
+	matrix.addRelative("monday");
+	matrix.addRelative("daring");
+	matrix.addRelative("india");
+	matrix.addRelative("tuberculosis");
+	matrix.addRelative("magician");
+	matrix.addRelative("fabrication");
+	matrix.addRelative("intensity");
+	matrix.addRelative("electrical");
+	matrix.addRelative("magnificent");
+	$scope.matrix = matrix.createCells(matrix.calculateGrid(matrix.crossRelations()));
 }]);
