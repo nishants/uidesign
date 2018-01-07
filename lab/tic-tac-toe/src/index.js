144b814c13d766e4150c8d40c9aba564d786e6d2
nishant <nishant.singh87@gmail.com>
2018-01-07 11:08:29 +0530

    Finish game.

--- lab/tic-tac-toe/src/index.js
+++ lab/tic-tac-toe/src/index.js
@@ -14,44 +14,8 @@ class Square extends React.Component{
 }
 
 class Board extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      nextMove: "X",
-      gameEnded  : null,
-      squares: new Array(9).fill(null),
-      onClick: (squareIndex)=>{
-        if(!this.state.squares[squareIndex] && !this.state.gameEnded){
-          var squares    = this.state.squares.slice(),
-              fillSquare = this.state.nextMove,
-              winner     = null,
-              gameEnded  = false,
-              nextMove = fillSquare === "O" ? "X" : "O";
-          squares[squareIndex] = fillSquare;
-          winner = this.state.getWinner(squares);
-          gameEnded = winner || squares.indexOf(null) === -1;
-          gameEnded ? this.props.onGameEnd(winner): this.props.onNextMove(nextMove);
-          this.setState({squares: squares, nextMove: nextMove, gameEnded: gameEnded, winner: winner});
-        }
-      },
-      getWinner: (squares)=>{
-        let winner = (squares[0] === squares[1]) && (squares[0] === squares[2])  ? squares[0] : null;
-        winner = winner || ((squares[0] === squares[3]) && (squares[0] === squares[6]) ? squares[0] : null);
-        winner = winner || ((squares[0] === squares[4]) && (squares[0] === squares[8]) ? squares[0] : null);
-        winner = winner || ((squares[1] === squares[4]) && (squares[1] === squares[7]) ? squares[1] : null);
-        winner = winner || ((squares[2] === squares[5]) && (squares[2] === squares[8]) ? squares[2] : null);
-        winner = winner || ((squares[3] === squares[4]) && (squares[3] === squares[5]) ? squares[3] : null);
-        winner = winner || ((squares[6] === squares[7]) && (squares[6] === squares[8]) ? squares[6] : null);
-        winner = winner || ((squares[6] === squares[2]) && (squares[6] === squares[4]) ? squares[6] : null);
-        return winner;
-      }
-    }
-  }
   renderSquare(index){
-    return (<Square clickAction={()=> this.state.onClick(index)} squareValue={this.state.squares[index]}/>);
-  }
-
-  componentDidUpdate() {
+    return (<Square clickAction={()=> this.props.onNextMove(index)} squareValue={this.props.squares[index]}/>);
   }
   render(){
     return (
@@ -81,25 +45,84 @@ class Game extends React.Component {
     super(props);
     this.state = {
       status: null,
-      nextMove : (nextMove)=>{
-        this.setState({status: nextMove+ "'s turn"});
+      history: [],
+      nextMove: "X",
+      gameEnded  : null,
+      squares: new Array(9).fill(null),
+      getWinner: (squares)=>{
+        let winner = (squares[0] === squares[1]) && (squares[0] === squares[2])  ? squares[0] : null;
+        winner = winner || ((squares[0] === squares[3]) && (squares[0] === squares[6]) ? squares[0] : null);
+        winner = winner || ((squares[0] === squares[4]) && (squares[0] === squares[8]) ? squares[0] : null);
+        winner = winner || ((squares[1] === squares[4]) && (squares[1] === squares[7]) ? squares[1] : null);
+        winner = winner || ((squares[2] === squares[5]) && (squares[2] === squares[8]) ? squares[2] : null);
+        winner = winner || ((squares[3] === squares[4]) && (squares[3] === squares[5]) ? squares[3] : null);
+        winner = winner || ((squares[6] === squares[7]) && (squares[6] === squares[8]) ? squares[6] : null);
+        winner = winner || ((squares[6] === squares[2]) && (squares[6] === squares[4]) ? squares[6] : null);
+        return winner;
       },
-      onGameEnd: (winner)=>{
+      onNextMove : (squareIndex)=>{
+        if(!this.state.squares[squareIndex] && !this.state.gameEnded){
+          var squares    = this.state.squares.slice(),
+              fillSquare = this.state.nextMove,
+              winner     = null,
+              gameEnded  = false,
+              nextMove   = fillSquare === "O" ? "X" : "O",
+              history    = this.state.history.slice(),
+              status     = null;
+
+          history.push({
+            nextMove  : this.state.nextMove,
+            gameEnded : this.state.gameEnded,
+            squares   : this.state.squares,
+            status    : this.state.status,
+            winner    : this.state.winner,
+            history   : this.state.history,
+          });
+
+          squares[squareIndex] = fillSquare;
+          winner    = this.state.getWinner(squares);
+          gameEnded = winner || squares.indexOf(null) === -1;
+          status    = winner ? winner +" won" : (gameEnded ? "Game over" : nextMove + "'s turn");
+
+          this.setState({
+            nextMove  : nextMove,
+            gameEnded : gameEnded,
+            squares   : squares,
+            status    : status,
+            winner    : winner,
+            history   : history
+          });
+        }
+      },
+      restore: (state)=>{
         this.setState({
-          status: winner ? winner +" won" : "Game over"
+          nextMove  : state.nextMove,
+          gameEnded : state.gameEnded,
+          squares   : state.squares,
+          status    : state.status,
+          winner    : state.winner,
+          history   : state.history,
         });
       }
     }
+  }
+  getHistory(){
+    return this.state.history.map((state, moveIndex)=> {
+      return <li><button onClick={()=>{this.state.restore(state)}}>move#{moveIndex}</button></li>;
+    });
   }
   render() {
     return (
         <div className="game">
           <div className="game-board">
-            <Board onNextMove={this.state.nextMove} onGameEnd={this.state.onGameEnd}/>
+            <Board squares={this.state.squares}
+                   onNextMove={this.state.onNextMove}
+                   addToHistory={this.state.addHistory}
+                   onGameEnd={this.state.onGameEnd}/>
           </div>
           <div className="game-info">
             <div className="status">{this.state.status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{this.getHistory()}</ol>
           </div>
         </div>
   );
