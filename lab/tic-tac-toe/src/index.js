2642c1302fc61616e733f57973cfb806140e8429
nishant <nishant.singh87@gmail.com>
2018-01-07 19:33:56 +0530

    another implementation for tic tac toe.

--- lab/tic-tac-toe/src/index.js
+++ lab/tic-tac-toe/src/index.js
@@ -2,132 +2,103 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component{
-  render(){
-    return (
-        <button className="square"
-                onClick={this.props.clickAction}>
-          {this.props.squareValue}
-        </button>
-    );
-  }
-}
-
 class Board extends React.Component{
   renderSquare(index){
-    return (<Square clickAction={()=> this.props.onNextMove(index)} squareValue={this.props.squares[index]}/>);
+    return <div key={index} className='square' onClick={()=> this.props.selectSquare(index)}>{this.props.squares[index]}</div>
+  }
+  renderBoardRow(...indices){
+    return <div className="board-row">{indices.map((index)=> this.renderSquare(index))}</div>
   }
   render(){
     return (
         <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+          {this.renderBoardRow(0,1,2)}
+          {this.renderBoardRow(3,4,5)}
+          {this.renderBoardRow(6,7,8)}
         </div>
     );
   }
 }
 
-class Game extends React.Component {
+class Game extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      status: null,
       history: [],
-      nextMove: "X",
-      gameEnded  : null,
       squares: new Array(9).fill(null),
-      getWinner: (squares)=>{
-        let winner = (squares[0] === squares[1]) && (squares[0] === squares[2])  ? squares[0] : null;
-        winner = winner || ((squares[0] === squares[3]) && (squares[0] === squares[6]) ? squares[0] : null);
-        winner = winner || ((squares[0] === squares[4]) && (squares[0] === squares[8]) ? squares[0] : null);
-        winner = winner || ((squares[1] === squares[4]) && (squares[1] === squares[7]) ? squares[1] : null);
-        winner = winner || ((squares[2] === squares[5]) && (squares[2] === squares[8]) ? squares[2] : null);
-        winner = winner || ((squares[3] === squares[4]) && (squares[3] === squares[5]) ? squares[3] : null);
-        winner = winner || ((squares[6] === squares[7]) && (squares[6] === squares[8]) ? squares[6] : null);
-        winner = winner || ((squares[6] === squares[2]) && (squares[6] === squares[4]) ? squares[6] : null);
-        return winner;
-      },
-      onNextMove : (squareIndex)=>{
-        if(!this.state.squares[squareIndex] && !this.state.gameEnded){
-          var squares    = this.state.squares.slice(),
-              fillSquare = this.state.nextMove,
-              winner     = null,
-              gameEnded  = false,
-              nextMove   = fillSquare === "O" ? "X" : "O",
-              history    = this.state.history.slice(),
-              status     = null;
-
-          history.push({
-            nextMove  : this.state.nextMove,
-            gameEnded : this.state.gameEnded,
-            squares   : this.state.squares,
-            status    : this.state.status,
-            winner    : this.state.winner,
-            history   : this.state.history,
-          });
-
-          squares[squareIndex] = fillSquare;
-          winner    = this.state.getWinner(squares);
-          gameEnded = winner || squares.indexOf(null) === -1;
-          status    = winner ? winner +" won" : (gameEnded ? "Game over" : nextMove + "'s turn");
-
-          this.setState({
-            nextMove  : nextMove,
-            gameEnded : gameEnded,
-            squares   : squares,
-            status    : status,
-            winner    : winner,
-            history   : history
-          });
-        }
-      },
-      restore: (state)=>{
-        this.setState({
-          nextMove  : state.nextMove,
-          gameEnded : state.gameEnded,
-          squares   : state.squares,
-          status    : state.status,
-          winner    : state.winner,
-          history   : state.history,
-        });
-      }
-    }
+      nextMove: "X",
+      gameEnd : null
+    };
+    this.WINNING_COMBINATIONS = [
+        [0,1,2],
+        [0,3,6],
+        [0,4,8],
+        [3,4,5],
+        [6,7,8],
+        [6,4,2],
+        [1,4,7],
+        [2,5,8],
+    ];
   }
-  getHistory(){
-    return this.state.history.map((state, moveIndex)=> {
-      return <li key={moveIndex}><button  onClick={()=>{this.state.restore(state)}}>move#{moveIndex}</button></li>;
+  restore(state){
+    this.setState({
+      history : state.history.slice(),
+      squares : state.squares,
+      nextMove: state.nextMove,
+      gameEnd : state.gameEnd
+
     });
   }
-  render() {
+  getHistory(){
+    return this.state.history.map((state, index)=>{
+      return <li key={index}><button onClick={()=> this.restore(state)}>move#{index}</button></li>
+    });
+  }
+  getStatus(){
+    return this.state.gameEnd ? (this.state.gameEnd === "none" ? "Game Over !" : "Winner : " + this.state.gameEnd ) : ("Next move : " + this.state.nextMove);
+  }
+  getWinnerIn(squares){
+    var winningCombination = this.WINNING_COMBINATIONS.find((combination)=> {
+      return (squares[combination[0]] === squares[combination[1]]) && (squares[combination[0]] === squares[combination[2]])
+    });
+    return winningCombination ? squares[winningCombination[0]] : null;
+  }
+  selectSquare(index){
+    if(!this.state.squares[index] && !this.state.gameEnd){
+      let squares = this.state.squares.slice(),
+          history = this.state.history.slice(),
+          gameEnd = null;
+      squares[index] = this.state.nextMove;
+      gameEnd = this.getWinnerIn(squares) || (squares.indexOf(null) === -1 ? "none" : null);
+
+      history.push({
+        history : this.state.history.slice(),
+        squares : this.state.squares.slice(),
+        nextMove: this.state.nextMove,
+        gameEnd : this.state.gameEnd
+      });
+
+      this.setState({
+        history : history,
+        squares : squares,
+        nextMove: this.state.nextMove === "X" ? "O" : "X",
+        gameEnd : gameEnd
+      })
+    }
+  }
+  render(){
     return (
-        <div className="game">
-          <div className="game-board">
-            <Board squares={this.state.squares}
-                   onNextMove={this.state.onNextMove}/>
+        <div className='game'>
+          <div className='game-board'>
+            <Board squares={this.state.squares} selectSquare={(index)=>{this.selectSquare(index)}}/>
           </div>
-          <div className="game-info">
-            <div className="status">{this.state.status}</div>
-            <ol>{this.getHistory()}</ol>
-          </div>
+          <div className='game-info'>
+            <div className='status'>{this.getStatus()}</div>
+            <ul className='history'>{this.getHistory()}</ul>
+            </div>
         </div>
-  );
+    )
   }
 }
 
-ReactDOM.render(<Game/>,document.getElementById("root"));
-
-
-
+ReactDOM.render(<Game/>, document.getElementById('root'));
