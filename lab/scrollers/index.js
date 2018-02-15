9979a0181d8d2fd56292fa0df5fc8a8067a9d7d7
nishant <nishant.singh87@gmail.com>
2018-02-15 22:03:41 +0530

    create scroller.

---
+++ lab/scrollers/index.js
@@ -0,0 +1,11 @@
+app = angular.module("scroller", []);
+app.controller("ScrollerController",["$scope", function($scope){
+  var states = [
+    {name: "one-sketch"},
+    {name: "two-begin"},
+    {name: "three-expand"},
+    {name: "four-fill"},
+  ];
+  $scope.states = states;
+  $scope.scroller = {};
+}])
