afa7375fea7d7de25270e46c75f7975bc0ef48ee
nishant <nishant.singh87@gmail.com>
2018-02-16 00:25:14 +0530

    full animation .

--- lab/scrollers/index.js
+++ lab/scrollers/index.js
@@ -1,6 +1,7 @@
 app = angular.module("scroller", []);
 app.controller("ScrollerController",["$scope", function($scope){
   var states = [
+    {name: "zero"},
     {name: "one-sketch"},
     {name: "two-begin"},
     {name: "three-expand"},
